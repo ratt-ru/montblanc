19acd5ea292a371a2a784b46b07a1e47b5e5c7b0
Simon Perkins <simon.perkins@gmail.com>
2014-01-15 17:01:37 +0200

    Now that we have a proper serial kernel, try paralellise it a 3D block

--- pycuda_pipeline/pipedrimes.py
+++ pycuda_pipeline/pipedrimes.py
@@ -291,63 +291,91 @@ __global__ void predict(
     pycuda::complex<double> * VisIn,
     double * UVWin,
     double * LM,
-    int ndir, int nchan)
+    long * A0,
+    long * A1,
+    double * wavelength,
+    pycuda::complex<double> * jones,
+    int ndir, int nchan, int na, int nrows)
 {
-    const int i = blockIdx.x*blockDim.x + threadIdx.x;
+    // Our space of visibilities is a 3D matrix of BL x DDE x CHAN
+    // This is the output
 
-    const pycuda::complex<double> I = pycuda::complex<double>(0,1);
-    const pycuda::complex<double> c0 = 2.0*CUDART_PI_F*I;
+    #define CUDA_XDIM blockDim.x*gridDim.x
+    #define CUDA_YDIM blockDim.y*gridDim.y
+    #define CUDA_ZDIM blockDim.z*gridDim.z
 
-    double wavelength[1024];
+    #define SLICE_STRIDE CUDA_XDIM*CUDA_YDIM
+    #define ROW_STRIDE CUDA_YDIM
 
-    const double refwave = 3e8;
+    const pycuda::complex<double> I = pycuda::complex<double>(0.,1.);
+    const pycuda::complex<double> c0 = 2.0*CUDART_PI*I;
+    const double refwave = 1e6;
 
-    pycuda::complex<double> * p0 = VisIn;
-    double * p1 = UVWin;
+    // Baseline
+    const int BL = blockIdx.x*blockDim.x + threadIdx.x;
+    // Direction Dependent Effect
+    const int DDE = blockIdx.y*blockDim.y + threadIdx.y;
+    // Channel/Frequency
+    const int CHAN = blockIdx.z*blockDim.z + threadIdx.z;
 
-    return;
+    // Index into the visibility matrix
+    const int i = BL*SLICE_STRIDE + DDE*ROW_STRIDE + CHAN; 
 
-    for(int dd=0;dd<ndir;dd++){
-        double l=LM[0];
-        double m=LM[1];
-        double fI=LM[2]; // flux
-        double alpha=LM[3];
-        double fQ=LM[4];
-        double fU=LM[5];
-        double fV=LM[6];        
-        double n = sqrt(1.-l*l-m*m)-1.;
+    // Our space of jone's matrices is a 3D matrix of ANTENNA x DDE x CHAN
+    // This is our input. We choose ANTENNA as our major axis.
+    const pycuda::complex<double> * ant0_jones = jones +
+        (A0[BL]*SLICE_STRIDE + DDE*ROW_STRIDE + CHAN)*4;
+    const pycuda::complex<double> * ant1_jones = jones +
+        (A1[BL]*SLICE_STRIDE + DDE*ROW_STRIDE + CHAN)*4;
 
-        LM+=6;
-  
-        VisIn = p0;
-        UVWin = p1;
+    // Coalesced loads should occur here!
+    double l = LM[DDE+0];
+    double m = LM[DDE+1];
+    double fI = LM[DDE+2];
+    double alpha = LM[DDE+3];
+    double fQ = LM[DDE+4];
+    double fU = LM[DDE+5];
+    double fV = LM[DDE+6];
 
-        pycuda::complex<double> sky[4] =
-        {
-            fI+fQ,
-            fU+I*fV,
-            fU-I*fV,
-            fI-fQ
-        };
+    double n = sqrt(1.0 - l*l - m*m) - 1.0;
 
-        pycuda::complex<double> JJ[4];
+    pycuda::complex<double> sky[4] =
+    {
+        fI+fQ,
+        fU+I*fV,
+        fU-I*fV,
+        fI-fQ
+    };
 
-        double phase = UVWin[0]*l + UVWin[1]*m + UVWin[2]*n;
-        UVWin += 3;
+    // Coalesced load should occur here!
+    double u = UVWin[BL+0];
+    double v = UVWin[BL+1];
+    double w = UVWin[BL+2];
 
-        for(int ch=0; ch<nchan; ch++){ 
-            pycuda::complex<double> c1=c0/wavelength[ch];
-            double thisflux=pow(refwave/wavelength[ch],alpha);
-            pycuda::complex<double> result=thisflux*pycuda::exp(phase*c1);
+    double phase = u*l + v*m + w*n;
 
-            //Sols shape: Nd, Nf, Na, 4
-//            J0=Sols+ dd*na*nchan*4 + ch*na*4 + A0[i]*4;
-//            J1=Sols+ dd*na*nchan*4 + ch*na*4 + A1[i]*4;
-        }
-    }    
+    pycuda::complex<double> c1 = c0 / wavelength[CHAN];
+    double flux = pow(refwave/wavelength[CHAN],alpha);
+    pycuda::complex<double> result = flux*pycuda::exp(c1*phase);
+
+    pycuda::complex<double> result_jones[4];
+
+    // Internals of Product2by2 should produce coalesced loads
+    Product2by2(ant0_jones, ant1_jones, result_jones);
+
+    VisIn[i+0] += result_jones[0]*result;
+    VisIn[i+1] += result_jones[1]*result;
+    VisIn[i+2] += result_jones[2]*result;
+    VisIn[i+3] += result_jones[3]*result;
+
+    #undef SLICE_STRIDE
+    #undef ROW_STRIDE
+
+    #undef CUDA_XDIM
+    #undef CUDA_YDIM
+    #undef CUDA_ZDIM
 }
 """)
-
         self.kernel = self.mod.get_function('predict')
 
     def shutdown(self, shared_data):
@@ -355,19 +383,56 @@ __global__ void predict(
     def pre_execution(self, shared_data):
         pass
     def execute(self, shared_data):
-        N = 1024
-        samples = 200
-        vis = (np.random.random(N*3) + np.random.random(N*3)*1j).astype(np.complex128)
-        uvw = np.random.random(N*3).astype(np.float64)
-        lmfa = np.random.random(samples*4).astype(np.float64)
-        nchan = 32
+        ## Here I define my data, and my Jones matrices
+        na=10        # Number of antenna
+        nrow=10      # Number of rows
+        nchan=20     # Number of channels
+        ndir=20      # Number of directions
 
-        vis_gpu = gpuarray.to_gpu_async(vis, stream=shared_data.stream[0])
+        # Visibilities ! has to have double complex
+        Vis=np.complex128(np.zeros((nrow,nchan,4)))
+        # UVW coordinates
+        uvw=np.float64(np.arange(nrow*3).reshape((nrow,3)))
+
+        # Frequencies in Hz
+        freqs=np.float64(np.linspace(1e6,2e6,nchan))
+        wavelength=3e8/freqs
+        # Sky coordinates
+        l=np.float64(np.random.randn(ndir)*0.1)
+        m=np.float64(np.random.randn(ndir)*0.1)
+        fI=np.float64(np.ones((ndir,)))
+        alpha=np.float64(np.zeros((ndir,)))
+        fV=np.float64(np.ones((ndir,)))
+        fU=np.float64(np.ones((ndir,)))
+        fQ=np.float64(np.ones((ndir,)))
+        lms=(np.array([l,m,fI,alpha,fV,fU,fQ]).T).copy().astype(np.float64)
+
+        # Antennas
+        A0=np.int64(np.random.rand(nrow)*na)
+        A1=np.int64(np.random.rand(nrow)*na)
+
+        # Jones matrices
+        Sols=np.complex128(np.random.randn(ndir,nchan,na,4)+1j*np.random.randn(ndir,nchan,na,4))
+
+        # Matrix containing information, here just the reference frequency
+        # to estimate the flux from spectral index
+        Info=np.array([1e6],np.float64)
+
+#        P1=predict.predictSols(Vis, A0, A1, uvw, lms, WaveL, Sols, Info)
+
+        vis_gpu = gpuarray.to_gpu_async(Vis, stream=shared_data.stream[0])
         uvw_gpu = gpuarray.to_gpu_async(uvw, stream=shared_data.stream[0])
-        lmfa_gpu = gpuarray.to_gpu_async(lmfa, stream=shared_data.stream[0])
+        lms_gpu = gpuarray.to_gpu_async(lms, stream=shared_data.stream[0])
+        A0_gpu = gpuarray.to_gpu_async(A0, stream=shared_data.stream[0])
+        A1_gpu = gpuarray.to_gpu_async(A1, stream=shared_data.stream[0])
+        wavelength_gpu = gpuarray.to_gpu_async(wavelength, stream=shared_data.stream[0])
+        Sols_gpu = gpuarray.to_gpu_async(Sols, stream=shared_data.stream[0])
 
-        self.kernel(vis_gpu, uvw_gpu, lmfa_gpu, np.int32(samples), np.int32(nchan),
-            block=(512,1,1), stream=shared_data.stream[0], grid=(1,1))
+        self.kernel(vis_gpu, uvw_gpu, lms_gpu,
+            A0_gpu, A1_gpu, wavelength_gpu, Sols_gpu,
+            np.int32(ndir), np.int32(nchan),
+            np.int32(na), np.int32(nrow),
+            stream=shared_data.stream[0], block=(8,8,8), grid=(1,1,1))
 
     def post_execution(self, shared_data):
         pass
@@ -566,3 +631,131 @@ def main(argv=None):
 
 if __name__ == "__main__":
     sys.exit(main())
+
+
+serial_predict = self.mod = SourceModule("""
+#include <pycuda-complex.hpp>
+#include \"math_constants.h\"
+
+__device__ void Product2by2(
+    const pycuda::complex<double> * lhs,
+    const pycuda::complex<double> * rhs,
+    pycuda::complex<double> * result)
+{
+    const pycuda::complex<double> & a00 = lhs[0];
+    const pycuda::complex<double> & a10 = lhs[2];
+    const pycuda::complex<double> & a01 = lhs[1];
+    const pycuda::complex<double> & a11 = lhs[3];
+
+    const pycuda::complex<double> & b00 = rhs[0];
+    const pycuda::complex<double> & b10 = rhs[2];
+    const pycuda::complex<double> & b01 = rhs[1];
+    const pycuda::complex<double> & b11 = rhs[3];
+
+    result[0]=a00*b00+a01*b10;
+    result[1]=a00*b01+a01*b11;
+    result[2]=a10*b00+a11*b10;
+    result[3]=a10*b01+a11*b11;
+}
+
+__device__ void Product2by2H(
+    const pycuda::complex<double> * lhs,
+    const pycuda::complex<double> * rhs,
+    pycuda::complex<double> * result)
+{
+    const pycuda::complex<double> & a00 = lhs[0];
+    const pycuda::complex<double> & a10 = lhs[2];
+    const pycuda::complex<double> & a01 = lhs[1];
+    const pycuda::complex<double> & a11 = lhs[3];
+
+    const pycuda::complex<double> b00 = pycuda::conj(rhs[0]);
+    const pycuda::complex<double> b10 = pycuda::conj(rhs[2]);
+    const pycuda::complex<double> b01 = pycuda::conj(rhs[1]);
+    const pycuda::complex<double> b11 = pycuda::conj(rhs[3]);
+
+    result[0]=a00*b00+a01*b10;
+    result[1]=a00*b01+a01*b11;
+    result[2]=a10*b00+a11*b10;
+    result[3]=a10*b01+a11*b11;
+}
+
+
+__global__ void predict(
+    pycuda::complex<double> * VisIn,
+    double * UVWin,
+    double * LM,
+    long * A0,
+    long * A1,
+    double * wavelength,
+    pycuda::complex<double> * solution,
+    int ndir, int nchan, int na, int nrows)
+{
+//    const int i = blockIdx.x*blockDim.x + threadIdx.x;
+
+    const pycuda::complex<double> I = pycuda::complex<double>(0,1);
+    const pycuda::complex<double> c0 = 2.0*CUDART_PI_F*I;
+
+    const double refwave = 1e6;
+
+    pycuda::complex<double> * p0 = VisIn;
+    double * p1 = UVWin;
+
+    return;
+
+    for(int dd=0;dd<ndir;dd++)
+    {
+        double l=LM[0];
+        double m=LM[1];
+        double fI=LM[2]; // flux
+        double alpha=LM[3];
+        double fQ=LM[4];
+        double fU=LM[5];
+        double fV=LM[6];        
+        double n = sqrt(1.-l*l-m*m)-1.;
+
+        LM+=6;
+        VisIn = p0;
+        UVWin = p1;
+
+        pycuda::complex<double> sky[4] =
+        {
+            fI+fQ,
+            fU+I*fV,
+            fU-I*fV,
+            fI-fQ
+        };
+
+        double phase = UVWin[0]*l + UVWin[1]*m + UVWin[2]*n;
+        UVWin += 3;
+    #if 1
+
+        for(int bl=0; bl<nrows; ++bl)
+        {
+            for(int ch=0; ch<nchan; ++ch)
+            { 
+                pycuda::complex<double> c1=c0/wavelength[ch];
+                double this_flux=pow(refwave/wavelength[ch],alpha);
+                pycuda::complex<double> result=this_flux*pycuda::exp(phase*c1);
+
+                pycuda::complex<double> JJ[4];
+                //Sols shape: Nd, Nf, Na, 4
+                pycuda::complex<double> * J0
+                    = solution + dd*na*nchan*4 + ch*na*4 + A0[bl]*4;
+                pycuda::complex<double> * J1
+                    = solution + dd*na*nchan*4 + ch*na*4 + A1[bl]*4;
+
+                Product2by2(J0, J1, JJ);
+
+                VisIn[0] += JJ[0]*result;
+                VisIn[1] += JJ[1]*result;
+                VisIn[2] += JJ[2]*result;
+                VisIn[3] += JJ[3]*result;
+
+                VisIn += 4;
+            }
+        }
+
+    #endif
+    }    
+}
+""")
