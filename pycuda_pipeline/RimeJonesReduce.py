4e78c8fdfb1c985f53f1bbddd8d7d7b2c7339951
Simon Perkins <simon.perkins@gmail.com>
2014-02-10 16:18:47 +0200

    Save an example of the float test case actually working for some reason

--- pycuda_pipeline/RimeJonesReduce.py
+++ pycuda_pipeline/RimeJonesReduce.py
@@ -3,48 +3,16 @@ import pycuda
 import pycuda.curandom
 import pycuda.driver as cuda
 import pycuda.gpuarray as gpuarray
-from pycuda.compiler import SourceModule
-from pycuda.tools import dtype_to_ctype
 
 from node import *
 
 import segreduce
 
-KERNEL_TEMPLATE = """
-#include <pycuda-helpers.hpp>
-
-//#define BLOCK_SIZE %(block_size)d
-//#define WARP_SIZE %(warp_size)d
-
-//typedef %(value_type)s value_type;
-//typedef %(index_type)s index_type;
-
-__global__ void seg_reduce_sum()
-{
-    
-}
-
-"""
-
 class RimeJonesReduce(Node):
     def __init__(self):
         super(RimeJonesReduce, self).__init__()
     def initialise(self, shared_data):
-        self.mod = SourceModule(KERNEL_TEMPLATE % {
-                # Huge assumption here. The handle sitting in
-                # the stream object is a CUStream type.
-                # (Check the stream class in src/cpp/cuda.hpp).
-                # mgpu::CreateCudaDeviceAttachStream in KERNEL_TEMPLATE
-                # wants a cudaStream_t. However, according to the following
-                #  http://developer.download.nvidia.com/compute/cuda/4_1/rel/toolkit/docs/online/group__CUDART__DRIVER.html
-                # 'The types CUstream and cudaStream_t are identical and may be used interchangeably.'
-                'stream_handle' : shared_data.stream[0].handle,
-                'block_size' : 256,
-                'warp_size' : 32,
-                'value_type' : dtype_to_ctype(np.float64),
-                'index_type' : dtype_to_ctype(np.int32)
-            },
-            no_extern_c=1)
+        pass
     def shutdown(self, shared_data):
         pass
     def pre_execution(self, shared_data):
@@ -68,11 +36,14 @@ class RimeJonesReduce(Node):
 #                keys[i,bl] = (bl*jones_shape[0] + i)*jones_shape[2]
         """
 
-        N = njones = 1024*1024*1024
+        N = njones = 64
         jones = np.arange(N).astype(np.complex128);
-        keys = np.array([0, 7, 8])
+        jones.imag = jones.real
+        #jones = np.empty([N],dtype=np.complex128)
+        keys = np.array([0, 15, 31, 63],dtype=np.int32)
 
 
+        """
         n_threads = 256
         n_blocks = (njones + n_threads - 1) / n_threads
         block, grid = (n_threads,1,1), (n_blocks,1,1)
@@ -82,54 +53,37 @@ class RimeJonesReduce(Node):
 
         print 'njones=', njones, 'stream=', shared_data.stream[0].handle#, \
 #            'gpudata=', int(shared_data.jones_gpu.gpudata)
+        """
 
         jones_gpu = gpuarray.to_gpu_async(jones, stream=shared_data.stream[0])
         keys_gpu = gpuarray.to_gpu_async(keys, stream=shared_data.stream[0])
         sums_gpu = gpuarray.zeros(shape=keys.shape, dtype=jones_gpu.dtype.type)
 
         # http://blog.gmane.org/gmane.comp.python.cuda/month=20131101
-
+        # At present, this creates a new context, which is not ideal
         segreduce.segmented_reduce_complex128_sum(
-            jones_gpu,
-            keys_gpu,
-            sums_gpu,
-            device_id=0,
-            stream=shared_data.stream[0],
-            test_ptr=int(shared_data.jones_gpu.gpudata))
+            jones_gpu, keys_gpu, sums_gpu,
+            device_id=0, stream=shared_data.stream[0])
 
         sums = sums_gpu.get_async(stream=shared_data.stream[0])
         print sums
 
-        jones = np.arange(N).astype(np.float32);
-        keys = np.array([0, 7, 20])
+        jones = np.arange(64).astype(np.float32);
+        keys = np.array([0, 15, 31, 63],dtype=np.int32)
 
-        n_threads = 256
-        n_blocks = (njones + n_threads - 1) / n_threads
-        block, grid = (n_threads,1,1), (n_blocks,1,1)
-
-        print 'jones=', jones
-        print 'keys=', keys
-
-        print 'njones=', njones, 'stream=', shared_data.stream[0].handle#, \
-#            'gpudata=', int(shared_data.jones_gpu.gpudata)
+        print 'jones=', jones, 'keys=', keys
+        print 'njones=', njones, 'stream=', shared_data.stream[0].handle
 
         jones_gpu = gpuarray.to_gpu_async(jones, stream=shared_data.stream[0])
         keys_gpu = gpuarray.to_gpu_async(keys, stream=shared_data.stream[0])
-        sums_gpu = gpuarray.zeros(shape=keys.shape, dtype=jones_gpu.dtype.type)
-
-        # http://blog.gmane.org/gmane.comp.python.cuda/month=20131101
+        sums_gpu = gpuarray.zeros(shape=keys.shape, dtype=jones.dtype.type)
 
         segreduce.segmented_reduce_float32_sum(
-            jones_gpu,
-            keys_gpu,
-            sums_gpu,
-            device_id=0,
-            stream=shared_data.stream[0],
-            test_ptr=int(shared_data.jones_gpu.gpudata))
+            jones_gpu, keys_gpu, sums_gpu,
+            device_id=0, stream=shared_data.stream[0])
 
         sums = sums_gpu.get_async(stream=shared_data.stream[0])
-        print sums
-
+        print 'sums', sums
 
     def post_execution(self, shared_data):
         pass
