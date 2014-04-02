1635352a88fc2858d797b71f78a427d60b8de068
Simon Perkins <simon.perkins@gmail.com>
2014-04-02 15:39:50 +0200

    Added a floating point reduction class

--- RimeJonesReduce.py
+++ RimeJonesReduce.py
@@ -26,3 +26,22 @@ class RimeJonesReduce(Node):
 
     def post_execution(self, shared_data):
         pass
+
+class RimeJonesReduceFloat(Node):
+    def __init__(self):
+        super(RimeJonesReduceFloat, self).__init__()
+    def initialise(self, shared_data):
+        pass
+    def shutdown(self, shared_data):
+        pass
+    def pre_execution(self, shared_data):
+        pass
+    def execute(self, shared_data):
+        sd = shared_data
+
+        crimes.segmented_reduce_complex64_sum(
+            data=sd.jones_gpu, seg_starts=sd.keys_gpu,
+            seg_sums=sd.vis_gpu, device_id=0)
+
+    def post_execution(self, shared_data):
+        pass        
