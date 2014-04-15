- Corrected the baseline calculation. nbl = na*(na-1)/2. This removes the autocorrelations and the MeasurementSet files seem to handle things this way too. - Added an array representing the antenna pair for each baseline. - Used this array in the EBK kernels. - Zeroed the GPU arrays on creation instead of creating them empty. - Actually set the antenna pairs, UVW coordinates and wavelengths in the MeasurementSetSharedData constructor. - Added a test_reduce_float test case.

BaseSharedData.py
@@ -26,7 +26,7 @@ class BaseSharedData(SharedData):
         # - timesteps
         # - sources
         self.na = na
-        self.nbl = nbl = (self.na**2 + self.na)/2
+        self.nbl = nbl = (na*(na-1))/2
         self.nchan = nchan
         self.ntime = ntime
         self.nsrc = nsrc
@@ -44,7 +44,7 @@ class BaseSharedData(SharedData):
 
         # Set up input data shapes
         self.uvw_shape = (3, nbl, ntime)
-        self.ant_pairs_shape = (2, nbl, ntime)
+        self.ant_pairs_shape = (2, nbl)
         self.lm_shape = (2, nsrc)
         self.brightness_shape = (5, nsrc)
         self.wavelength_shape = (nchan)
@@ -74,6 +74,17 @@ class BaseSharedData(SharedData):
         """ Set the chi squared result. Useful for sensibly initialising it """
         self.X2 = self.ft(X2)
 
+    def get_default_ant_pairs(self):
+        """
+        Return an np.array(shape=(2, nbl), dtype=np.int32]) containing the
+        default antenna pairs for each baseline.
+        """
+        # Create the antenna pair mapping, from upper triangle indices
+        # based on the number of antenna. 
+        ant_pairs = np.array(np.triu_indices(self.na,1)).astype(np.int32)
+        assert ant_pairs.shape == self.ant_pairs_shape
+        return ant_pairs
+
     def __str__(self):
         return "RIME Simulation Dimensions" + \
             "\nAntenna:       " + str(self.na) + \
@@ -88,6 +99,7 @@ class GPUSharedData(BaseSharedData):
     for holding simulation input and output.
     """
     uvw_gpu = ArrayData()
+    ant_pairs_gpu = ArrayData()
     lm_gpu = ArrayData()
     brightness_gpu = ArrayData()
     wavelength_gpu = ArrayData()
@@ -102,21 +114,23 @@ class GPUSharedData(BaseSharedData):
         super(GPUSharedData, self).__init__(na,nchan,ntime,nsrc,dtype)
 
         # Create the input data arrays on the GPU
-        self.uvw_gpu = gpuarray.empty(shape=self.uvw_shape,dtype=self.ft)
-        self.lm_gpu = gpuarray.empty(shape=self.lm_shape,dtype=self.ft)
-        self.brightness_gpu = gpuarray.empty(shape=self.brightness_shape,dtype=self.ft)
-        self.wavelength_gpu = gpuarray.empty(shape=self.wavelength_shape,dtype=self.ft)
-        self.point_errors_gpu = gpuarray.empty(shape=self.point_errors_shape,dtype=self.ft)
-        self.bayes_model_gpu = gpuarray.empty(shape=self.bayes_model_shape,dtype=self.ct)
+        self.uvw_gpu = gpuarray.zeros(shape=self.uvw_shape,dtype=self.ft)
+        self.ant_pairs_gpu = gpuarray.zeros(shape=self.ant_pairs_shape,dtype=np.int32)
+        self.lm_gpu = gpuarray.zeros(shape=self.lm_shape,dtype=self.ft)
+        self.brightness_gpu = gpuarray.zeros(shape=self.brightness_shape,dtype=self.ft)
+        self.wavelength_gpu = gpuarray.zeros(shape=self.wavelength_shape,dtype=self.ft)
+        self.point_errors_gpu = gpuarray.zeros(shape=self.point_errors_shape,dtype=self.ft)
+        self.bayes_model_gpu = gpuarray.zeros(shape=self.bayes_model_shape,dtype=self.ct)
 
         # Create the output data arrays on the GPU
-        self.jones_gpu = gpuarray.empty(shape=self.jones_shape,dtype=self.ct)
-        self.vis_gpu = gpuarray.empty(shape=self.vis_shape,dtype=self.ct)
-        self.chi_sqrd_result_gpu = gpuarray.empty(shape=self.chi_sqrd_result_shape,dtype=self.ft)
+        self.jones_gpu = gpuarray.zeros(shape=self.jones_shape,dtype=self.ct)
+        self.vis_gpu = gpuarray.zeros(shape=self.vis_shape,dtype=self.ct)
+        self.chi_sqrd_result_gpu = gpuarray.zeros(shape=self.chi_sqrd_result_shape,dtype=self.ft)
 
         # Create a list of the GPU arrays
         self.gpu_data = [
             self.uvw_gpu,
+            self.ant_pairs_gpu,
             self.lm_gpu,
             self.brightness_gpu,
             self.wavelength_gpu,
@@ -132,6 +146,9 @@ class GPUSharedData(BaseSharedData):
 
     def transfer_uvw(self,uvw):
         self.uvw_gpu.set(uvw)
+
+    def transfer_ant_pairs(self, ant_pairs):
+        self.ant_pairs_gpu.set(ant_pairs)
 
     def transfer_lm(self,lm):
         self.lm_gpu.set(lm)

MeasurementSetSharedData.py
@@ -6,6 +6,7 @@ import pycuda.gpuarray as gpuarray
 from pyrap.tables import table
 
 from RimeBKFloat import *
+from RimeEBKFloat import *
 from RimeJonesReduce import *
 from RimeChiSquaredFloat import *
 from RimeChiSquaredReduceFloat import *
@@ -33,7 +34,7 @@ class MeasurementSetSharedData(GPUSharedData):
         t = table(self.ms_file, ack=False)
 
         # Get the UVW coordinates
-        self.uvw=t.getcol("UVW").T.astype(dtype)
+        uvw=t.getcol("UVW").T.astype(dtype)
 
         # Open the antenna table
         ta=table(self.ms_file + os.sep + MeasurementSetSharedData.ANTENNA_TABLE, ack=False)
@@ -42,18 +43,14 @@ class MeasurementSetSharedData(GPUSharedData):
         f=tf.getcol("CHAN_FREQ").astype(dtype)
 
         na = len(ta.getcol("NAME"))
-        nbl = (na**2+na)/2
+        nbl = (na*(na-1))/2
         nchan = f.size
-        ntime = self.uvw.shape[1] / nbl
+        ntime = uvw.shape[1] / nbl
 
         super(MeasurementSetSharedData, self).__init__(\
             na=na,nchan=nchan,ntime=ntime,nsrc=nsrc,dtype=dtype)
 
         self.wavelength = 3e8/f
-    	# TODO: Setting the reference wavelength to a frequency makes no sense,
-    	# but this matches Cyril's predict...
-    	# First dimension also seems to be of size 1 here...
-    	self.set_refwave(f[0][nchan/2])
 
         # Create the key positions. This snippet creates an array
         # equal to the list of positions of the last array element timestep)
@@ -61,12 +58,22 @@ class MeasurementSetSharedData(GPUSharedData):
             *self.jones_shape[-1]).astype(np.int32)
         self.keys_gpu = gpuarray.to_gpu(self.keys)
 
+        self.uvw = uvw.reshape(self.uvw_shape).copy()
+
+		# Transfer the uvw coordinates 
+        # and antenna pairs to the GPU
+        self.transfer_uvw(uvw)
+        self.transfer_ant_pairs(self.get_default_ant_pairs())
+        self.transfer_wavelength(self.wavelength)
+
+        # TODO: Setting the reference wavelength to a frequency makes no sense,
+        # but this matches Cyril's predict...
+        # First dimension also seems to be of size 1 here...
+        self.set_refwave(f[0][nchan/2])
+
         t.close()
         ta.close()
         tf.close()
-
-    def get_visibilities(self):
-        return self.vis_gpu.get()
 
 if __name__ == '__main__':
     import sys
@@ -84,7 +91,7 @@ if __name__ == '__main__':
     # Create a pipeline consisting of an EBK kernel, followed by a reduction,
 	# a chi squared difference between the Bayesian Model and the Visibilities
 	# and a further reduction to produce the Chi Squared Value
-    pipeline = PipedRimes([RimeBKFloat(), RimeJonesReduceFloat(), RimeChiSquaredFloat(), RimeChiSquaredReduceFloat()])
+    pipeline = PipedRimes([RimeEBKFloat(), RimeJonesReduceFloat(), RimeChiSquaredFloat(), RimeChiSquaredReduceFloat()])
 	# Initialise the pipeline
     pipeline.initialise(sd)
 
@@ -113,7 +120,6 @@ if __name__ == '__main__':
 
     kernels_start, kernels_end = cuda.Event(), cuda.Event()
     time_sum = 0.0
-    count = 10
 
     # Execute the pipeline
     for i in range(args.count):
@@ -131,12 +137,11 @@ if __name__ == '__main__':
         time_sum += kernels_start.time_till(kernels_end)
 
         # The chi squared result is set on the shared data object
-        print sd.X2
+        print 'Chi Squared Value', sd.X2
 
         # Obtain the visibilities  (slow)
-        #V = sd.get_visibilities()
+        #V = sd.vis_gpu.get()
 
-    print sd
     print 'kernels: elapsed time: %gms' %\
         (time_sum / args.count)
 

RimeEBK.py
@@ -18,6 +18,7 @@ void rime_jones_EBK(
     double * brightness,
     double * wavelength,
     double * point_error,
+	int * ant_pairs,
     double2 * jones,
     double refwave, 
     int nbl, int nchan, int ntime, int nsrc, int na)
@@ -30,10 +31,6 @@ void rime_jones_EBK(
     int CHAN = (blockIdx.y*blockDim.y + threadIdx.y) / ntime;
     int TIME = (blockIdx.y*blockDim.y + threadIdx.y) % ntime;
     int BL = blockIdx.z*blockDim.z + threadIdx.z;
-    // Calculates the antenna pairs from the baseline!
-    int ANT1 = int(floor((sqrtf(1+8*BL)-1)/2));
-    int ANT2 = BL - (ANT1*ANT1+ANT1)/2;
-    ANT1 += 1;
 
     if(BL >= nbl || SRC >= nsrc || CHAN >= nchan || TIME >= ntime)
         return;
@@ -61,15 +58,18 @@ void rime_jones_EBK(
     // Index
     int i;
 
-    if(threadIdx.x == 0)
+    // Varies by time (y) and antenna (baseline) (z) 
+    //if(threadIdx.x == 0)
     {
-        i = ANT1*ntime + TIME; ld_p[threadIdx.z] = point_error[i];
-        i += na*ntime;         md_p[threadIdx.z] = point_error[i];
-        i = ANT2*ntime + TIME; ld_q[threadIdx.z] = point_error[i];
-        i += na*ntime;         md_q[threadIdx.z] = point_error[i];
+        int ANT1 = ant_pairs[BL]; int ANT2 = ant_pairs[BL+nbl];
+        i = ANT1*ntime + TIME;     ld_p[threadIdx.z] = point_error[i];
+        i += na*ntime;             md_p[threadIdx.z] = point_error[i];
+        i = ANT2*ntime + TIME;     ld_q[threadIdx.z] = point_error[i];
+        i += na*ntime;             md_q[threadIdx.z] = point_error[i];
     }
 
-    if(threadIdx.z == 0)
+    // Varies by source (x)
+    if(threadIdx.y == 0 && threadIdx.z == 0)
     {
         i = SRC;   l[threadIdx.x] = LM[i]; fI[threadIdx.x] = brightness[i];
         i += nsrc; m[threadIdx.x] = LM[i]; fQ[threadIdx.x] = brightness[i];
@@ -77,7 +77,8 @@ void rime_jones_EBK(
         i += nsrc; fV[threadIdx.x] = brightness[i];        
     }
 
-   if(threadIdx.y == 0)
+    // Varies by channel (y)
+    if(threadIdx.x == 0 && threadIdx.z == 0)
     {
         i = CHAN; wave[threadIdx.y] = wavelength[i];
     }
@@ -183,17 +184,17 @@ class RimeEBK(Node):
     def get_kernel_params(self, shared_data):
         sd = shared_data
 
-        baselines_per_block = 8 if sd.nbl > 8 else sd.nbl
         srcs_per_block = 32 if sd.nsrc > 32 else sd.nsrc
         time_chans_per_block = 1
+        baselines_per_block = 8 if sd.nbl > 8 else sd.nbl
 
-        baseline_blocks = (sd.nbl + baselines_per_block - 1) / baselines_per_block
         src_blocks = (sd.nsrc + srcs_per_block - 1) / srcs_per_block
         time_chan_blocks = sd.ntime*sd.nchan
+        baseline_blocks = (sd.nbl + baselines_per_block - 1) / baselines_per_block
 
         return {
-            'block' : (baselines_per_block,srcs_per_block,1), \
-            'grid'  : (baseline_blocks,src_blocks,time_chan_blocks), \
+            'block' : (srcs_per_block,time_chans_per_block,baselines_per_block), \
+            'grid'  : (src_blocks,time_chan_blocks,baseline_blocks), \
             'shared' : (4*baselines_per_block + \
                         6*srcs_per_block + \
                         1*time_chans_per_block)*\
@@ -201,12 +202,11 @@ class RimeEBK(Node):
 
     def execute(self, shared_data):
         sd = shared_data
-        params = self.get_kernel_params(sd)
 
         self.kernel(sd.uvw_gpu, sd.lm_gpu, sd.brightness_gpu,
-            sd.wavelength_gpu, sd.point_errors_gpu, sd.jones_gpu, sd.refwave,
+            sd.wavelength_gpu, sd.point_errors_gpu, sd.ant_pairs_gpu, sd.jones_gpu , sd.refwave,
             np.int32(sd.nbl), np.int32(sd.nchan), np.int32(sd.ntime), np.int32(sd.nsrc),
-		    np.int32(sd.na), **params)
+            np.int32(sd.na), **self.get_kernel_params(sd))       
 
     def post_execution(self, shared_data):
         pass

RimeEBKFloat.py
@@ -18,6 +18,7 @@ void rime_jones_EBK_float(
     float * brightness,
     float * wavelength,
     float * point_error,
+    int * ant_pairs,
     float2 * jones,
     float refwave,
     int nbl, int nchan, int ntime, int nsrc, int na)
@@ -31,10 +32,6 @@ void rime_jones_EBK_float(
     int CHAN = (blockIdx.y*blockDim.y + threadIdx.y) / ntime;
     int TIME = (blockIdx.y*blockDim.y + threadIdx.y) % ntime;
     int BL = blockIdx.z*blockDim.z + threadIdx.z;
-    // Calculates the antenna pairs from the baseline!
-    int ANT1 = int(floor((sqrtf(1+8*BL)-1)/2));
-    int ANT2 = BL - (ANT1*ANT1+ANT1)/2;
-    ANT1 += 1;
 
     if(BL >= nbl || SRC >= nsrc || CHAN >= nchan || TIME >= ntime)
         return;
@@ -63,16 +60,13 @@ void rime_jones_EBK_float(
     int i;
 
     // Varies by time (y) and antenna (baseline) (z) 
-    if(threadIdx.x == 0)
+    //if(threadIdx.x == 0)
     {
-        i = ANT1*ntime + TIME; ld_p[threadIdx.z] = point_error[i];
-        i += na*ntime;         md_p[threadIdx.z] = point_error[i];
-        i = ANT2*ntime + TIME; ld_q[threadIdx.z] = point_error[i];
-        i += na*ntime;         md_q[threadIdx.z] = point_error[i];
-
-#if __CUDA_ARCH__ >= 200
-        printf(\"%f %f %f %f\\n\", ld_p[threadIdx.z], md_p[threadIdx.z], ld_q[threadIdx.z], md_q[threadIdx.z]);
-#endif
+        int ANT1 = ant_pairs[BL]; int ANT2 = ant_pairs[BL+nbl];
+        i = ANT1*ntime + TIME;     ld_p[threadIdx.z] = point_error[i];
+        i += na*ntime;             md_p[threadIdx.z] = point_error[i];
+        i = ANT2*ntime + TIME;     ld_q[threadIdx.z] = point_error[i];
+        i += na*ntime;             md_q[threadIdx.z] = point_error[i];
     }
 
     // Varies by source (x)
@@ -190,17 +184,17 @@ class RimeEBKFloat(Node):
     def get_kernel_params(self, shared_data):
         sd = shared_data
 
-        baselines_per_block = 8 if sd.nbl > 8 else sd.nbl
         srcs_per_block = 32 if sd.nsrc > 32 else sd.nsrc
         time_chans_per_block = 1
+        baselines_per_block = 8 if sd.nbl > 8 else sd.nbl
 
-        baseline_blocks = (sd.nbl + baselines_per_block - 1) / baselines_per_block
         src_blocks = (sd.nsrc + srcs_per_block - 1) / srcs_per_block
         time_chan_blocks = sd.ntime*sd.nchan
+        baseline_blocks = (sd.nbl + baselines_per_block - 1) / baselines_per_block
 
         return {
-            'block' : (baselines_per_block,srcs_per_block,1), \
-            'grid'  : (baseline_blocks,src_blocks,time_chan_blocks), \
+            'block' : (srcs_per_block,time_chans_per_block,baselines_per_block), \
+            'grid'  : (src_blocks,time_chan_blocks,baseline_blocks), \
             'shared' : (4*baselines_per_block + \
                         6*srcs_per_block + \
                         1*time_chans_per_block)*\
@@ -210,7 +204,7 @@ class RimeEBKFloat(Node):
         sd = shared_data
 
         self.kernel(sd.uvw_gpu, sd.lm_gpu, sd.brightness_gpu,
-            sd.wavelength_gpu, sd.point_errors_gpu, sd.jones_gpu, sd.refwave,
+            sd.wavelength_gpu, sd.point_errors_gpu, sd.ant_pairs_gpu, sd.jones_gpu, sd.refwave,
             np.int32(sd.nbl), np.int32(sd.nchan), np.int32(sd.ntime), np.int32(sd.nsrc),
             np.int32(sd.na), **self.get_kernel_params(sd))       
 

TestRimes.py
@@ -106,19 +106,16 @@ class TestRimes(unittest.TestCase):
         rime_sum.shutdown(sd)
 
     def test_EBK_float(self):
-        sd = TestSharedData(na=2,nchan=2,ntime=2,nsrc=2,dtype=np.float32)      
+        sd = TestSharedData(na=10,nchan=32,ntime=10,nsrc=200,dtype=np.float32)       
         rime_ebk = RimeEBKFloat()
         rime_bk = RimeBKFloat()
 
         rime_ebk.initialise(sd)
         rime_bk.initialise(sd)
 
-        print 'point_errors', sd.point_errors
-
         # Invoke the BK kernel
         rime_bk.execute(sd)
         jones_gpu = sd.jones_gpu.get()
-        print 'BK float', jones_gpu
 
         for bl in range(sd.nbl):
             ANT1 = int(np.floor((np.sqrt(1+8*bl)-1)/2))
@@ -142,7 +139,7 @@ class TestRimes(unittest.TestCase):
 
         rime_ebk.execute(sd)        
         jones_gpu = sd.jones_gpu.get()
-        print 'EBK float', jones_gpu
+        self.assertTrue(np.isnan(jones_gpu).any() == False)
 
         rime_ebk.shutdown(sd)
         rime_bk.shutdown(sd)
@@ -274,6 +271,41 @@ class TestRimes(unittest.TestCase):
         vis_gpu = gpuarray.empty(shape=keys.shape, dtype=jones.dtype.type)
 
         crimes.segmented_reduce_complex128_sum(
+            data=jones_gpu, seg_starts=keys_gpu, seg_sums=vis_gpu,
+            device_id=0)
+
+        # Shutdown the rime node, we don't need it any more
+        rime_reduce.shutdown(sd)
+
+        # Add everything along the last axis (time)
+        vis_cpu = np.sum(jones,axis=len(sd.jones_shape)-1)
+
+        # Confirm similar results
+        self.assertTrue(np.allclose(vis_cpu.flatten(), vis_gpu.get()))
+
+    def test_reduce_float(self):
+        sd = TestSharedData(na=10,nchan=32,ntime=10,nsrc=200,dtype=np.float32)       
+        rime_reduce = RimeJonesReduce()
+
+        rime_reduce.initialise(sd)
+
+        # Create the jones matrices
+        jsize = np.product(sd.jones_shape)
+        jones = (np.random.random(jsize) + 1j*np.random.random(jsize))\
+            .astype(sd.ct).reshape(sd.jones_shape)
+
+        # Create the key positions. This snippet creates an array
+        # equal to the list of positions of the last array element timestep)
+        keys = (np.arange(np.product(sd.jones_shape[:-1]))*sd.jones_shape[-1])\
+            .astype(np.int32)
+        
+        # Send the jones and keys to the gpu, and create the output array for
+        # the visibilities.
+        jones_gpu = gpuarray.to_gpu(jones.flatten())
+        keys_gpu = gpuarray.to_gpu(keys)
+        vis_gpu = gpuarray.empty(shape=keys.shape, dtype=jones.dtype.type)
+
+        crimes.segmented_reduce_complex64_sum(
             data=jones_gpu, seg_starts=keys_gpu, seg_sums=vis_gpu,
             device_id=0)
 