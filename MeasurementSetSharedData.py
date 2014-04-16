ae5f3321ea9cabaadc56c881ac2a883f292840db
Simon Perkins <simon.perkins@gmail.com>
2014-04-16 14:56:35 +0200

    - Autocorrelated baselines are now stripped from Measurement Sets when reading. - The baseline antenna pairs are now read in from the Measurement Set, instead of being hardcoded. - The reference frequency is read from the Measurement Set, instead of being deduced from the spectral frequency.

--- MeasurementSetSharedData.py
+++ MeasurementSetSharedData.py
@@ -20,7 +20,9 @@ class MeasurementSetSharedData(GPUSharedData):
         self.ms_file = ms_file
 
         # Open the measurement set
-        t = table(self.ms_file, ack=False)
+        ms = table(self.ms_file, ack=False)
+        # Strip out all auto-correlated baselines
+        t = ms.query('ANTENNA1 != ANTENNA2')
 
         # Get the UVW coordinates
         uvw=t.getcol("UVW").T.astype(dtype)
@@ -31,34 +33,55 @@ class MeasurementSetSharedData(GPUSharedData):
         tf=table(self.ms_file + os.sep + MeasurementSetSharedData.SPECTRAL_WINDOW, ack=False)
         f=tf.getcol("CHAN_FREQ").astype(dtype)
 
+        # Determine the problem dimensions
         na = len(ta.getcol("NAME"))
-        nbl = (na*(na-1))/2
+        nbl = (na*(na-1))//2
         nchan = f.size
-        ntime = uvw.shape[1] / nbl
+        ntime = uvw.shape[1] // nbl
+
+        # Check that we're getting the correct shape...
+        expected_uvw_shape = (3, nbl*ntime)
+
+        if expected_uvw_shape != uvw.shape:
+            raise ValueError, 'uvw.shape %s != expected %s' % (uvw.shape,expected_uvw_shape)
 
         super(MeasurementSetSharedData, self).__init__(\
             na=na,nchan=nchan,ntime=ntime,nsrc=nsrc,dtype=dtype)
 
-        self.wavelength = 3e8/f
+        # Reshape the flatten array
+        uvw = uvw.reshape(self.uvw_shape).copy()
+
+        # Determine the wavelengths
+        wavelength = 3e8/f[0]
+
+        # Get the baseline antenna pairs
+        ant1 = t.getcol('ANTENNA1')
+        ant2 = t.getcol('ANTENNA2')
+
+        expected_ant_shape = (nbl*ntime,)
+        
+        if expected_ant_shape != ant1.shape:
+            raise ValueError, 'ANTENNA1 shape is %s != expected %s' % (ant1.shape,expected_ant_shape)
+
+        if expected_ant_shape != ant2.shape:
+            raise ValueError, 'ANTENNA2 shape is %s != expected %s' % (ant2.shape,expected_ant_shape)
+
+        ant_pairs = np.vstack((ant1,ant2)).reshape(self.ant_pairs_shape)
+
+        # Transfer the uvw coordinates, antenna pairs and wavelengths to the GPU
+        self.transfer_uvw(uvw)
+        self.transfer_ant_pairs(ant_pairs)
+        self.transfer_wavelength(wavelength)
+
+        # TODO: Setting reference wavelength equal to frequency makes no sense
+        # First dimension also seems to be of size 1 here...
+        self.set_refwave(tf.getcol('REF_FREQUENCY')[0])
 
         # Create the key positions. This snippet creates an array
         # equal to the list of positions of the last array element timestep)
         self.keys = (np.arange(np.product(self.jones_shape[:-1]))
             *self.jones_shape[-1]).astype(np.int32)
         self.keys_gpu = gpuarray.to_gpu(self.keys)
-
-        self.uvw = uvw.reshape(self.uvw_shape).copy()
-
-		# Transfer the uvw coordinates 
-        # and antenna pairs to the GPU
-        self.transfer_uvw(uvw)
-        self.transfer_ant_pairs(self.get_default_ant_pairs())
-        self.transfer_wavelength(self.wavelength)
-
-        # TODO: Setting the reference wavelength to a frequency makes no sense,
-        # but this matches Cyril's predict...
-        # First dimension also seems to be of size 1 here...
-        self.set_refwave(f[0][nchan/2])
 
         t.close()
         ta.close()
