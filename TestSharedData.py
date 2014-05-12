430fc6dd2693fffa061f3635b963abadc09c906b
Simon Perkins <simon.perkins@gmail.com>
2014-05-12 15:46:37 +0200

    Used the shortened 'sd' variable, instead of 'self' when constructing TestSharedData.

--- TestSharedData.py
+++ TestSharedData.py
@@ -10,7 +10,7 @@ class TestSharedData(GPUSharedData):
         ft,ct = sd.ft,sd.ct
 
         # Baseline coordinates in the u,v,w (frequency) domain
-        self.uvw = np.array([ \
+        sd.uvw = np.array([ \
             np.arange(1,nbl*ntime+1).reshape(nbl,ntime).astype(ft)*3., \
             np.arange(1,nbl*ntime+1).reshape(nbl,ntime).astype(ft)*2., \
             np.arange(1,nbl*ntime+1).reshape(nbl,ntime).astype(ft)*1.], \
@@ -19,7 +19,7 @@ class TestSharedData(GPUSharedData):
         # Point source coordinates in the l,m,n (sky image) domain
         l=ft(np.random.random(nsrc)*0.1)
         m=ft(np.random.random(nsrc)*0.1)
-        self.lm=np.array([l,m], dtype=ft)
+        sd.lm=np.array([l,m], dtype=ft)
 
         # Brightness matrix for the point sources
         fI=ft(np.ones((nsrc,)))
@@ -27,45 +27,45 @@ class TestSharedData(GPUSharedData):
         fU=ft(np.random.random(nsrc)*0.5)
         fV=ft(np.random.random(nsrc)*0.5)
         alpha=ft(np.random.random(nsrc)*0.1)
-        self.brightness = np.array([fI,fQ,fU,fV,alpha], dtype=ft)
+        sd.brightness = np.array([fI,fQ,fU,fV,alpha], dtype=ft)
 
         # Generate nchan frequencies/wavelengths
     	frequencies = ft(np.linspace(1e6,2e6,nchan))
-        self.wavelength = 3e8/frequencies
-        self.set_ref_freq(frequencies[nchan/2])
+        sd.wavelength = 3e8/frequencies
+        sd.set_ref_freq(frequencies[nchan/2])
 
         # Generate the antenna pointing errors
-        self.point_errors = np.random.random(np.product(self.point_errors_shape))\
-            .astype(ft).reshape(self.point_errors_shape)
+        sd.point_errors = np.random.random(np.product(sd.point_errors_shape))\
+            .astype(ft).reshape(sd.point_errors_shape)
 
         # Copy the uvw, lm and brightness data to the gpu
-        self.transfer_uvw(self.uvw)
-        self.transfer_ant_pairs(self.get_default_ant_pairs())
-        self.transfer_lm(self.lm)
-        self.transfer_brightness(self.brightness)
-        self.transfer_wavelength(self.wavelength)
-        self.transfer_point_errors(self.point_errors)
+        sd.transfer_uvw(sd.uvw)
+        sd.transfer_ant_pairs(sd.get_default_ant_pairs())
+        sd.transfer_lm(sd.lm)
+        sd.transfer_brightness(sd.brightness)
+        sd.transfer_wavelength(sd.wavelength)
+        sd.transfer_point_errors(sd.point_errors)
 
         # Create the key positions. This snippet creates an array
         # equal to the list of positions of the last array element timestep)
-        self.keys = (np.arange(np.product(self.jones_shape[:-1]))
-            *self.jones_shape[-1]).astype(np.int32)
-        self.keys_gpu = gpuarray.to_gpu(self.keys)
+        sd.keys = (np.arange(np.product(sd.jones_shape[:-1]))
+            *sd.jones_shape[-1]).astype(np.int32)
+        sd.keys_gpu = gpuarray.to_gpu(sd.keys)
 
         # Output visibility matrix
-        assert np.product(self.vis_shape) == np.product(self.keys.shape)
-        nviselements = np.product(self.vis_shape)
-        self.vis = (np.random.random(nviselements) + np.random.random(nviselements)*1j)\
-            .astype(ct).reshape(self.vis_shape)
-        self.transfer_vis(self.vis)
+        assert np.product(sd.vis_shape) == np.product(sd.keys.shape)
+        nviselements = np.product(sd.vis_shape)
+        sd.vis = (np.random.random(nviselements) + np.random.random(nviselements)*1j)\
+            .astype(ct).reshape(sd.vis_shape)
+        sd.transfer_vis(sd.vis)
 
         # The bayesian model
-        assert np.product(self.bayes_model_shape) == np.product(self.keys.shape)
-        nbayes = np.product(self.bayes_model_shape)
-        self.bayes_model = (np.random.random(nbayes) + np.random.random(nbayes)*1j)\
-            .astype(ct).reshape(self.bayes_model_shape)
-        self.transfer_bayes_model(self.bayes_model)
-        self.set_sigma_sqrd((np.random.random(1)**2).astype(ft)[0])
+        assert np.product(sd.bayes_model_shape) == np.product(sd.keys.shape)
+        nbayes = np.product(sd.bayes_model_shape)
+        sd.bayes_model = (np.random.random(nbayes) + np.random.random(nbayes)*1j)\
+            .astype(ct).reshape(sd.bayes_model_shape)
+        sd.transfer_bayes_model(sd.bayes_model)
+        sd.set_sigma_sqrd((np.random.random(1)**2).astype(ft)[0])
 
     def compute_bk_jones(self):
         sd = self
