Added numpy code to test the EBK terms. - Added compute_ebk_jones() to TestSharedData. - Add some asserts into compute_bk_jones() - Fixed the some E term computation in the GPU code. - set_cos3_constant() allows the specification of the cos^3 constant in the E term. - Changed some sqrt's to sqrtf's in the float kernels.

RimeEBK.py
@@ -20,12 +20,11 @@ void rime_jones_EBK(
     double * point_error,
 	int * ant_pairs,
     double2 * jones,
-    double ref_freq, 
+    double ref_freq,
+    double cos3_constant, 
     int nbl, int nchan, int ntime, int nsrc, int na)
 {
     // Our data space is a 4D matrix of BL x CHAN x TIME x SRC
-    #define COS3_CONST 65*1e-9
-
     // Baseline, Source, Channel and Time indices
     int SRC = blockIdx.x*blockDim.x + threadIdx.x;
     int CHAN = (blockIdx.y*blockDim.y + threadIdx.y) / ntime;
@@ -115,17 +114,17 @@ void rime_jones_EBK(
     i = SRC+nsrc*4; phase = pow(ref_freq/wave[threadIdx.y], brightness[i]);
     real *= phase; imag *= phase;
 
-    double E_p = (l[threadIdx.x]+ld_p[threadIdx.z])*(l[threadIdx.x]*ld_p[threadIdx.z]);
-    E_p += (m[threadIdx.x]+md_p[threadIdx.z])*(m[threadIdx.x]*md_p[threadIdx.z]);
+    double E_p = (l[threadIdx.x]-ld_p[threadIdx.z])*(l[threadIdx.x]-ld_p[threadIdx.z]);
+    E_p += (m[threadIdx.x]-md_p[threadIdx.z])*(m[threadIdx.x]-md_p[threadIdx.z]);
     E_p = sqrt(E_p);
-    E_p = cos(COS3_CONST*wave[threadIdx.y]*E_p);
+    E_p = cos(cos3_constant*wave[threadIdx.y]*E_p);
     E_p = E_p*E_p*E_p;
     real *= E_p; imag *= E_p;
 
-    double E_q = (l[threadIdx.x]+ld_q[threadIdx.z])*(l[threadIdx.x]*ld_q[threadIdx.z]);
-    E_q += (m[threadIdx.x]+md_q[threadIdx.z])*(m[threadIdx.x]*md_q[threadIdx.z]);
+    double E_q = (l[threadIdx.x]-ld_q[threadIdx.z])*(l[threadIdx.x]-ld_q[threadIdx.z]);
+    E_q += (m[threadIdx.x]-md_q[threadIdx.z])*(m[threadIdx.x]-md_q[threadIdx.z]);
     E_q = sqrt(E_q);
-    E_q = cos(COS3_CONST*wave[threadIdx.y]*E_q);
+    E_q = cos(cos3_constant*wave[threadIdx.y]*E_q);
     E_q = E_q*E_q*E_q;
     real *= E_q; imag *= E_q;
 
@@ -207,7 +206,8 @@ class RimeEBK(Node):
         sd = shared_data
 
         self.kernel(sd.uvw_gpu, sd.lm_gpu, sd.brightness_gpu,
-            sd.wavelength_gpu, sd.point_errors_gpu, sd.ant_pairs_gpu, sd.jones_gpu , sd.ref_freq,
+            sd.wavelength_gpu, sd.point_errors_gpu, sd.ant_pairs_gpu, sd.jones_gpu,
+            sd.ref_freq, sd.cos3_constant,
             np.int32(sd.nbl), np.int32(sd.nchan), np.int32(sd.ntime), np.int32(sd.nsrc),
             np.int32(sd.na), **self.get_kernel_params(sd))       
 

RimeEBKFloat.py
@@ -21,12 +21,10 @@ void rime_jones_EBK_float(
     int * ant_pairs,
     float2 * jones,
     float ref_freq,
+    float cos3_constant,
     int nbl, int nchan, int ntime, int nsrc, int na)
 {
     // Our data space is a 4D matrix of BL x SRC x CHAN x TIME
-
-    #define COS3_CONST 65*1e-9
-
     // Baseline, Source, Channel and Time indices
     int SRC = blockIdx.x*blockDim.x + threadIdx.x;
     int CHAN = (blockIdx.y*blockDim.y + threadIdx.y) / ntime;
@@ -93,7 +91,7 @@ void rime_jones_EBK_float(
     // n = sqrt(1.0 - l*l - m*m) - 1.0
     float phase = 1.0 - l[threadIdx.x]*l[threadIdx.x];
     phase -= m[threadIdx.x]*m[threadIdx.x];
-    phase = sqrt(phase) - 1.0;
+    phase = sqrtf(phase) - 1.0;
     // TODO: remove this superfluous variable
     // It only exists for debugging purposes
     // float n = phase;
@@ -116,17 +114,17 @@ void rime_jones_EBK_float(
     i = SRC+nsrc*4; phase = __powf(ref_freq/wave[threadIdx.y], brightness[i]);
     real *= phase; imag *= phase;
 
-    float E_p = (l[threadIdx.x]+ld_p[threadIdx.z])*(l[threadIdx.x]*ld_p[threadIdx.z]);
-    E_p += (m[threadIdx.x]+md_p[threadIdx.z])*(m[threadIdx.x]*md_p[threadIdx.z]);
-    E_p = sqrt(E_p);
-    E_p = __cosf(COS3_CONST*wave[threadIdx.y]*E_p);
+    float E_p = (l[threadIdx.x]-ld_p[threadIdx.z])*(l[threadIdx.x]-ld_p[threadIdx.z]);
+    E_p += (m[threadIdx.x]-md_p[threadIdx.z])*(m[threadIdx.x]-md_p[threadIdx.z]);
+    E_p = sqrtf(E_p);
+    E_p = __cosf(cos3_constant*wave[threadIdx.y]*E_p);
     E_p = E_p*E_p*E_p;
     real *= E_p; imag *= E_p;
 
-    float E_q = (l[threadIdx.x]+ld_q[threadIdx.z])*(l[threadIdx.x]*ld_q[threadIdx.z]);
-    E_q += (m[threadIdx.x]+md_q[threadIdx.z])*(m[threadIdx.x]*md_q[threadIdx.z]);
-    E_q = sqrt(E_q);
-    E_q = __cosf(COS3_CONST*wave[threadIdx.y]*E_q);
+    float E_q = (l[threadIdx.x]-ld_q[threadIdx.z])*(l[threadIdx.x]-ld_q[threadIdx.z]);
+    E_q += (m[threadIdx.x]-md_q[threadIdx.z])*(m[threadIdx.x]-md_q[threadIdx.z]);
+    E_q = sqrtf(E_q);
+    E_q = __cosf(cos3_constant*wave[threadIdx.y]*E_q);
     E_q = E_q*E_q*E_q;
     real *= E_q; imag *= E_q;
 
@@ -207,7 +205,8 @@ class RimeEBKFloat(Node):
         sd = shared_data
 
         self.kernel(sd.uvw_gpu, sd.lm_gpu, sd.brightness_gpu,
-            sd.wavelength_gpu, sd.point_errors_gpu, sd.ant_pairs_gpu, sd.jones_gpu, sd.ref_freq,
+            sd.wavelength_gpu, sd.point_errors_gpu, sd.ant_pairs_gpu, sd.jones_gpu,
+            sd.ref_freq, sd.cos3_constant,
             np.int32(sd.nbl), np.int32(sd.nchan), np.int32(sd.ntime), np.int32(sd.nsrc),
             np.int32(sd.na), **self.get_kernel_params(sd))       
 

TestRimes.py
@@ -82,13 +82,38 @@ class TestRimes(unittest.TestCase):
 
         # Invoke the BK kernel
         rime_bk.execute(sd)
-
-        jones_gpu = sd.jones_gpu.get()
-
-        rime_ebk.execute(sd)
+        rime_ebk.execute(sd)        
 
         rime_ebk.shutdown(sd)
         rime_bk.shutdown(sd)
+
+        jones_gpu = sd.jones_gpu.get()
+        jones_cpu = sd.compute_ebk_jones()
+
+        self.assertTrue(np.allclose(jones_gpu, jones_cpu))
+
+    def test_EBK_float(self):
+#        sd = TestSharedData(na=10,nchan=32,ntime=10,nsrc=200,dtype=np.float32,
+#            device=pycuda.autoinit.device)
+        sd = TestSharedData(na=4,nchan=4,ntime=2,nsrc=5,dtype=np.float32,
+            device=pycuda.autoinit.device)
+        rime_ebk = RimeEBKFloat()
+        rime_bk = RimeBKFloat()
+
+        rime_ebk.initialise(sd)
+        rime_bk.initialise(sd)
+
+        # Invoke the BK kernel
+        rime_bk.execute(sd)
+        rime_ebk.execute(sd)        
+
+        rime_ebk.shutdown(sd)
+        rime_bk.shutdown(sd)
+
+        jones_gpu = sd.jones_gpu.get()
+        jones_cpu = sd.compute_ebk_jones()
+
+        self.assertTrue(np.allclose(jones_gpu, jones_cpu))
 
     def test_sum_float(self):
         sd = TestSharedData(na=14,nchan=32,ntime=36,nsrc=100,dtype=np.float32,
@@ -108,26 +133,6 @@ class TestRimes(unittest.TestCase):
         self.assertTrue(np.allclose(vis_cpu, sd.vis_gpu.get()))
 
         rime_sum.shutdown(sd)
-
-    def test_EBK_float(self):
-        sd = TestSharedData(na=10,nchan=32,ntime=10,nsrc=200,dtype=np.float32,
-            device=pycuda.autoinit.device)
-        rime_ebk = RimeEBKFloat()
-        rime_bk = RimeBKFloat()
-
-        rime_ebk.initialise(sd)
-        rime_bk.initialise(sd)
-
-        # Invoke the BK kernel
-        rime_bk.execute(sd)
-        rime_ebk.execute(sd)        
-
-
-
-
-        rime_ebk.shutdown(sd)
-        rime_bk.shutdown(sd)
-
 
     def test_EBK_sum_float(self):
         sd = TestSharedData(na=10,nchan=32,ntime=10,nsrc=100,dtype=np.float32,

TestSharedData.py
@@ -78,16 +78,22 @@ class TestSharedData(GPUSharedData):
 
         # u*l+v*m+w*n. Outer product creates array of dim nbl x ntime x nsrcs
         phase = (np.outer(sd.uvw[0], sd.lm[0]) + \
-            np.outer(sd.uvw[1], sd.lm[1]) + \
+        	np.outer(sd.uvw[1], sd.lm[1]) + \
             np.outer(sd.uvw[2],n))\
                 .reshape(sd.nbl, sd.ntime, sd.nsrc)
+        assert phase.shape == (sd.nbl, sd.ntime, sd.nsrc)        	
 
         # 2*pi*sqrt(u*l+v*m+w*n)/wavelength. Dim. nbl x nchan x ntime x nsrcs 
         phase = (2*np.pi*1j*phase)[:,np.newaxis,:,:]/w[np.newaxis,:,:,np.newaxis]
+        assert phase.shape == (sd.nbl, sd.nchan, sd.ntime, sd.nsrc)        	
+
         # Dim nchan x ntime x nsrcs 
         power = np.power(sd.ref_freq/w[:,:,np.newaxis], sd.brightness[4])
+        assert power.shape == (sd.nchan, sd.ntime, sd.nsrc)        	
+
         # This works due to broadcast! Dim nbl x nchan x ntime x nsrcs
         phase_term = power*np.exp(phase)
+        assert phase_term.shape == (sd.nbl, sd.nchan, sd.ntime, sd.nsrc)        	
 
         # Create the brightness matrix. Dim 4 x nsrcs
         brightness = sd.ct([
@@ -95,14 +101,102 @@ class TestSharedData(GPUSharedData):
             sd.brightness[2] + 1j*sd.brightness[3],     # fU + fV*1j
             sd.brightness[2] - 1j*sd.brightness[3],     # fU - fV*1j
             sd.brightness[0]-sd.brightness[1] + 0j])        # fI-fQ + 0j
+        assert brightness.shape == (4, sd.nsrc)
 
         # This works due to broadcast! Multiplies along
         # srcs axis of brightness. Dim 4 x nbl x nchan x ntime x nsrcs.
         jones_cpu = (phase_term[np.newaxis,:,:,:,:]* \
             brightness[:,np.newaxis, np.newaxis, np.newaxis,:])\
             .reshape((4, sd.nbl, sd.nchan, sd.ntime, sd.nsrc))
+        assert jones_cpu.shape == sd.jones_shape
 
         return jones_cpu
 
     def compute_bk_vis(self):
         return np.add.reduce(self.compute_bk_jones(), axis=4)        
+
+    def compute_ebk_jones(self):
+    	sd = self
+
+#    	print sd
+
+        ap = sd.get_default_ant_pairs().view()
+        pe = sd.point_errors.view()
+
+        ap.shape = (2,sd.nbl*sd.ntime)
+        pe.shape = (2,sd.na*sd.ntime)
+
+        ant0,ant1 = ap[0], ap[1]
+
+#        print ap.shape
+#
+#        print 'antenna_pairs', ap
+#        print 'point_errors', pe
+#        print 'point_errors', pe.shape
+
+#        print pe[0,:].shape
+#        print pe[1,:].shape
+#
+#        print pe[0,ant0].shape
+#        print pe[1,ant1].shape
+#
+#        print pe[0,ant0]
+#        print pe[1,ant0]
+#
+#        print pe[0,ant1]
+#        print pe[1,ant1]
+
+        # Pointing errors for the first antenna
+        ld_p = pe[0,ant0].reshape(sd.nbl,sd.ntime)
+        md_p = pe[1,ant0].reshape(sd.nbl,sd.ntime)
+
+        d_p = pe[:,ant0].reshape(2,sd.nbl,sd.ntime)
+
+        assert(d_p[0] == ld_p).all()
+        assert(d_p[1] == md_p).all()
+
+        # Pointing errors for the second antenna
+        ld_q = pe[0,ant1].reshape(sd.nbl,sd.ntime)
+        md_q = pe[1,ant1].reshape(sd.nbl,sd.ntime)
+
+        d_q = pe[:,ant1].reshape(2,sd.nbl,sd.ntime)
+
+        assert(d_q[0] == ld_q).all()
+        assert(d_q[1] == md_q).all()
+
+#        print sd.lm.shape, d_p.shape
+
+        # Broadcasting here produces, nbl x ntime x nsrc
+        l_off = sd.lm[0] - d_p[0,:,:,np.newaxis]
+        m_off = sd.lm[1] - d_p[1,:,:,np.newaxis]
+        #print 'l_off', l_off
+        #print 'm_off', m_off
+        E_p = np.sqrt(l_off**2 + m_off**2)
+
+        assert E_p.shape == (sd.nbl, sd.ntime, sd.nsrc)
+
+        # Broadcasting here produces, nbl x nchan x ntime x nsrc
+        E_p = E_p[:,np.newaxis,:,:]*sd.wavelength[np.newaxis,:,np.newaxis,np.newaxis]
+        E_p = np.cos(sd.cos3_constant*E_p)**3
+        print 'E_p', E_p
+
+        assert E_p.shape == (sd.nbl, sd.nchan, sd.ntime, sd.nsrc)
+
+        # Broadcasting here produces, nbl x ntime x nsrc
+        l_off = sd.lm[0] - d_p[0,:,:,np.newaxis]
+        m_off = sd.lm[1] - d_p[1,:,:,np.newaxis]
+        E_q = np.sqrt(l_off**2 + m_off**2)
+
+        assert E_q.shape == (sd.nbl, sd.ntime, sd.nsrc)
+
+        # Broadcasting here produces, nbl x nchan x ntime x nsrc
+        E_q = E_q[:,np.newaxis,:,:]*sd.wavelength[np.newaxis,:,np.newaxis,np.newaxis]
+        E_q = np.cos(sd.cos3_constant*E_q)**3
+
+        assert E_q.shape == (sd.nbl, sd.nchan, sd.ntime, sd.nsrc)
+
+        #print 'E_p', E_p
+        #print 'E_q', E_q
+
+        return self.compute_bk_jones() * E_p * E_q
+