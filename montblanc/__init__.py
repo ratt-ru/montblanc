7013433ac8551eaee7ac91dbdfe77bf12296b560
Simon Perkins <simon.perkins@gmail.com>
2014-05-19 14:29:41 +0200

    Changes to the __init__.py file. - Added some basic imports. - Added a get_biro_pipeline method.

---
+++ montblanc/__init__.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+from node import Node, NullNode
+from pipeline import Pipeline
+from BaseSharedData import GPUSharedData
+
+from RimeBKFloat import RimeBKFloat
+from RimeEBKFloat import RimeEBKFloat
+from RimeJonesReduce import RimeJonesReduceFloat
+from RimeChiSquaredFloat import RimeChiSquaredFloat
+from RimeChiSquaredReduceFloat import RimeChiSquaredReduceFloat
+
+from RimeBK import RimeBK
+from RimeEBK import RimeEBK
+from RimeJonesReduce import RimeJonesReduce
+#from RimeChiSquared import RimeChiSquared
+#from RimeChiSquaredReduce import RimeChiSquaredReduce
+
+from MeasurementSetSharedData import MeasurementSetSharedData
+
+def get_biro_pipeline(msfile, nsrc, device=None):
+	"""
+	Returns a pipeline and shared data tuple defining a pipeline
+	suitable for BIRO.
+
+	msfile - Name of the measurement set file.
+	nsrc - Number of sources.
+	device - PyCUDA device. If left blank, the default device will be selected.
+
+	returns: a (pipeline, shared_data) tuple
+	"""
+	if device is None:
+		import pycuda.autoinit
+		device=pycuda.autoinit.device
+
+	# Create a shared data object from the Measurement Set file
+	sd = MeasurementSetSharedData(msfile, nsrc=nsrc, dtype=np.float32,
+		device=device)
+	# Create a pipeline consisting of an EBK kernel, followed by a reduction,
+	# a chi squared difference between the Bayesian Model and the Visibilities
+	# and a further reduction to produce the Chi Squared Value
+	pipeline = Pipeline([
+		RimeEBKFloat(),
+		RimeJonesReduceFloat(),
+		RimeChiSquaredFloat(),
+		RimeChiSquaredReduceFloat()])
+
+	return pipeline, sd
