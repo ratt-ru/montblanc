238c2d1721929b49699187dbfdee649a5dd2ae37
Simon Perkins <simon.perkins@gmail.com>
2014-05-19 10:46:22 +0200

    Add a basic setup.py script.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='montblanc',
+      version='0.1',
+      description='GPU-accelerated RIME implementations. ',
+      url='http://github.com/ska-sa/montblanc',
+      author='Simon Perkins',
+      author_email='simon.perkins@gmail.com',
+      license='MIT',
+      packages=['montblanc'],
+      zip_safe=False)
