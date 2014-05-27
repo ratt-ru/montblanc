Fixed the GPU Chi Squared code factoring the sigma squared into the calculation.

montblanc/RimeChiSquaredFloat.py
@@ -14,7 +14,6 @@ void rime_chi_squared_diff(
     float2 * data_vis,
     float2 * model_vis,
     float * output,
-    float sigma_squared,
     int nbl, int nchan, int ntime)
 {
     // data_vis and model_vis matrix have dimensions
@@ -49,7 +48,7 @@ void rime_chi_squared_diff(
     delta.x -= model.x; delta.y -= model.y;
     sum.x += delta.x*delta.x; sum.y += delta.y*delta.y;
 
-    output[j] = (sum.x + sum.y)/sigma_squared;
+    output[j] = (sum.x + sum.y);
 }
 """
 
@@ -82,7 +81,7 @@ class RimeChiSquaredFloat(Node):
         sd = shared_data
 
         self.kernel(sd.vis_gpu, sd.bayes_data_gpu, \
-            sd.chi_sqrd_result_gpu, sd.sigma_sqrd, \
+            sd.chi_sqrd_result_gpu, \
             np.int32(sd.nbl), np.int32(sd.nchan), np.int32(sd.ntime), \
             **self.get_kernel_params(sd))
             

montblanc/TestRimes.py
@@ -225,7 +225,7 @@ class TestRimes(unittest.TestCase):
         d = sd.vis - sd.bayes_data
         # Reduces a dimension so that we have (nbl,nchan,ntime)
         # (XX.real^2 + XY.real^2 + YX.real^2 + YY.real^2) + ((XX.imag^2 + XY.imag^2 + YX.imag^2 + YY.imag^2))
-        chi_sqrd_cpu = (np.add.reduce(d.real**2,axis=0) + np.add.reduce(d.imag**2,axis=0))/sd.sigma_sqrd
+        chi_sqrd_cpu = (np.add.reduce(d.real**2,axis=0) + np.add.reduce(d.imag**2,axis=0))
         chi_sqrd_gpu = sd.chi_sqrd_result_gpu.get()
 
         assert np.allclose(chi_sqrd_cpu, chi_sqrd_gpu)