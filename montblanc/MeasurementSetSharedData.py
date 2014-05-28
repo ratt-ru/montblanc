ratt-ru/montblanc

commit 3d586a91c4d4a61c58411fbc40e85e2b8cb85486

Improve code organisation.

montblanc/MeasurementSetSharedData.py
@@ -30,13 +30,19 @@ class MeasurementSetSharedData(GPUSharedData):
 
         # Get the UVW coordinates
         uvw=t.getcol("UVW").T.astype(dtype)
+        # Check that we're getting the correct shape...
+        expected_uvw_shape = (3, nbl*ntime)
 
-        ant_path = os.path.join(self.ms_file, MeasurementSetSharedData.ANTENNA_TABLE)
-        freq_path = os.path.join(self.ms_file, MeasurementSetSharedData.SPECTRAL_WINDOW)
+        if expected_uvw_shape != uvw.shape:
+            raise ValueError, 'uvw.shape %s != expected %s' % (uvw.shape,expected_uvw_shape)
+
         # Open the antenna table
+        ant_path = os.path.join(self.ms_file, MeasurementSetSharedData.ANTENNA_TABLE)
         ta=table(ant_path, ack=False)
+
         # Open the spectral window table
         tf=table(freq_path, ack=False)
+        freq_path = os.path.join(self.ms_file, MeasurementSetSharedData.SPECTRAL_WINDOW)
         f=tf.getcol("CHAN_FREQ").astype(dtype)
 
         # Determine the problem dimensions
@@ -44,12 +50,6 @@ class MeasurementSetSharedData(GPUSharedData):
         nbl = BaseSharedData.get_nr_of_baselines(na)
         nchan = f.size
         ntime = uvw.shape[1] // nbl
-
-        # Check that we're getting the correct shape...
-        expected_uvw_shape = (3, nbl*ntime)
-
-        if expected_uvw_shape != uvw.shape:
-            raise ValueError, 'uvw.shape %s != expected %s' % (uvw.shape,expected_uvw_shape)
 
         super(MeasurementSetSharedData, self).__init__(\
             na=na,nchan=nchan,ntime=ntime,nsrc=nsrc,dtype=dtype)

commit f69bc5f7c289bfb4b472262bce96c5ad7833cf29

Fix problems caused by 3d586a91c4d4a61c58411fbc40e85e2b8cb85486.

montblanc/MeasurementSetSharedData.py
@@ -30,19 +30,14 @@ class MeasurementSetSharedData(GPUSharedData):
 
         # Get the UVW coordinates
         uvw=t.getcol("UVW").T.astype(dtype)
-        # Check that we're getting the correct shape...
-        expected_uvw_shape = (3, nbl*ntime)
-
-        if expected_uvw_shape != uvw.shape:
-            raise ValueError, 'uvw.shape %s != expected %s' % (uvw.shape,expected_uvw_shape)
 
         # Open the antenna table
         ant_path = os.path.join(self.ms_file, MeasurementSetSharedData.ANTENNA_TABLE)
         ta=table(ant_path, ack=False)
 
         # Open the spectral window table
-        tf=table(freq_path, ack=False)
         freq_path = os.path.join(self.ms_file, MeasurementSetSharedData.SPECTRAL_WINDOW)
+        tf=table(freq_path, ack=False)
         f=tf.getcol("CHAN_FREQ").astype(dtype)
 
         # Determine the problem dimensions
@@ -56,6 +51,12 @@ class MeasurementSetSharedData(GPUSharedData):
 
         # Check that nbl agrees with version from constructor
         assert nbl == self.nbl
+
+        # Check that we're getting the correct shape...
+        expected_uvw_shape = (3, nbl*ntime)
+
+        if expected_uvw_shape != uvw.shape:
+            raise ValueError, 'uvw.shape %s != expected %s' % (uvw.shape,expected_uvw_shape)
 
         # Reshape the flatten array
         uvw = uvw.reshape(self.uvw_shape).copy()