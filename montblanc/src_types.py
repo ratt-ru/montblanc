256990d244d83a66bca39a965220d91f935eb1e4
Simon Perkins <simon.perkins@gmail.com>
2015-07-08 14:45:40 +0200

    Associate source types with their numbering variable. e.g. 'point' => 'npsrc' in the SOURCE_VAR_TYPES dict.
    Add default_sources function for creating a default mapping
    of source types to number of sources. e.g.
    
    default_sources(point=10,gaussian=20) returns
    a dict {'point': 10, 'gaussian': 20, 'sersic': 0 }.
    
    Add sources_to_nr_vars function for translating the above
    mapping into a dictionary mapping numbering variables
    to number of sources. e.g.
    
    sources_to_nr_vars({'point': 10, 'gaussian': 20 })
    returns a dict {'npsrc':10, 'ngsrc':20, 'nssrc':0}.

--- montblanc/src_types.py
+++ montblanc/src_types.py
@@ -18,6 +18,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
+import numpy as np
+
 POINT_TYPE = 'point'
 GAUSSIAN_TYPE = 'gaussian'
 SERSIC_TYPE = 'sersic'
@@ -26,14 +28,73 @@ POINT_NR_VAR = 'npsrc'
 GAUSSIAN_NR_VAR = 'ngsrc'
 SERSIC_NR_VAR = 'nssrc'
 
-#SOURCE_TYPES = [POINT_TYPE, GAUSSIAN_TYPE, SERSIC_TYPE]
-#SOURCE_NR_VARS = [POINT_NR_VAR, GAUSSIAN_NR_VAR, SERSIC_NR_VAR]
+SOURCE_VAR_TYPES = {
+    POINT_TYPE : POINT_NR_VAR,
+    GAUSSIAN_TYPE : GAUSSIAN_NR_VAR,
+    SERSIC_TYPE : SERSIC_NR_VAR
+}
 
-SOURCES_TYPES = [
-    { 'name': POINT_TYPE, 'nr_var': POINT_NR_VAR },
-    { 'name': GAUSSIAN_TYPE, 'nr_var': GAUSSIAN_NR_VAR },
-    { 'name': SERSIC_TYPE, 'nr_var': SERSIC_NR_VAR },
-]
+def default_sources(**kwargs):
+    """
+    Returns a dictionary mapping source types
+    to number of sources. If the number of sources
+    for the source type is supplied in the kwargs
+    these will be placed in the dictionary.
 
-def default_src_cfg():
-    return {}
+    e.g. if we have 'point', 'gaussian' and 'sersic'
+    source types, then
+
+    default_sources(point=10, gaussian=20)
+
+    will return a dict {'point': 10, 'gaussian': 20, 'sersic': 0}
+    """
+    S = {}
+    total = 0
+
+    # Zero all source types
+    for k, v in SOURCE_VAR_TYPES.iteritems():
+        # Try get the number of sources for this source
+        # from the kwargs
+        value = kwargs.get(k, 0)
+
+        try:
+            value = int(value)
+        except ValueError:
+            raise TypeError(('Supplied value %s '
+                'for source %s cannot be '
+                'converted to an integer') % \
+                    (value, k))    
+
+        total += value
+        S[k] = value
+
+    # Add a point source if no others exist
+    if total == 0:
+        S[POINT_TYPE] = 1
+
+    return S
+
+def sources_to_nr_vars(sources):
+    """
+    Converts a source type to number of sources mapping into
+    a source numbering variable to number of sources mapping.
+
+    If, for example, we have 'point', 'gaussian' and 'sersic'
+    source types, then passing the following dict as an argument
+    
+    sources_to_nr_vars({'point':10, 'gaussian': 20})
+    
+    will return a new dict
+
+    {'npsrc': 10, 'ngsrc': 20, 'nssrc': 0 }
+    """
+
+    sources = default_sources(**sources)
+
+    try:
+        return { SOURCE_VAR_TYPES[name]: nr for name, nr in sources.iteritems() }
+    except KeyError as e:
+        raise KeyError((
+            'No source type ''%s'' is '
+            'registered. Valid source types '
+            'are %s') % (e, SOURCE_VAR_TYPES.keys()))
