cd941eff4fdb1047b8bc7650939d18a6ed9c6011
Simon Perkins <simon.perkins@gmail.com>
2015-08-04 17:27:02 +0200

    Update the MS_ones.py example to handle v4

--- montblanc/examples/MS_ones.py
+++ montblanc/examples/MS_ones.py
@@ -35,33 +35,41 @@ if __name__ == '__main__':
     parser.add_argument('-ng','--ngsrc',dest='ngsrc', type=int, default=0, help='Number of Gaussian Sources')
     parser.add_argument('-ns','--nssrc',dest='nssrc', type=int, default=0, help='Number of Gaussian Sources')
     parser.add_argument('-c','--count',dest='count', type=int, default=10, help='Number of Iterations')
+    parser.add_argument('-v','--version',dest='version', type=str, default='v4', help='version')
 
     args = parser.parse_args(sys.argv[1:])
 
     slvr_cfg = montblanc.rime_solver_cfg(msfile=args.msfile,
         sources=montblanc.sources(point=args.npsrc, gaussian=args.ngsrc, sersic=args.nssrc),
-        dtype='double', version=Options.VERSION_TWO)
+        dtype='double', version=args.version)
 
     # Get the solver.
     with montblanc.rime_solver(slvr_cfg) as slvr:
         # Create point sources at zeros
         l=slvr.ft(np.zeros(slvr.nsrc))
         m=slvr.ft(np.zeros(slvr.nsrc))
-        lm=mbu.shape_list([l,m],
-            shape=slvr.lm_shape, dtype=slvr.lm_dtype)
-
-        # Create 1Jy point sources
-        fI=slvr.ft(np.ones(slvr.ntime*slvr.nsrc)).reshape(slvr.ntime,slvr.nsrc)
-        fQ=slvr.ft(np.zeros(slvr.ntime*slvr.nsrc)).reshape(slvr.ntime,slvr.nsrc)
-        fU=slvr.ft(np.zeros(slvr.ntime*slvr.nsrc)).reshape(slvr.ntime,slvr.nsrc)
-        fV=slvr.ft(np.zeros(slvr.ntime*slvr.nsrc)).reshape(slvr.ntime,slvr.nsrc)
-        alpha=slvr.ft(np.zeros(slvr.ntime*slvr.nsrc)).reshape(slvr.ntime,slvr.nsrc)
-        brightness = mbu.shape_list([fI,fQ,fU,fV,alpha],
-            shape=slvr.brightness_shape, dtype=slvr.brightness_dtype)
+        lm=mbu.shape_list([l,m], shape=slvr.lm_shape, dtype=slvr.lm_dtype)
 
         slvr.transfer_lm(lm)
-        slvr.transfer_brightness(brightness)
 
+        # Create 1Jy point sources
+        if args.version in [Options.VERSION_TWO, Options.VERSION_THREE]:
+            brightness = np.empty(shape=slvr.brightness_shape, dtype=slvr.brightness_dtype)
+            brightness[0,:,:] = 1
+            brightness[1,:,:] = 0
+            brightness[2,:,:] = 0
+            brightness[3,:,:] = 0
+            brightness[4,:,:] = 0
+            slvr.transfer_brightness(brightness)            
+        elif args.version in [Options.VERSION_FOUR, Options.VERSION_FIVE]:
+            stokes = np.empty(shape=slvr.stokes_shape, dtype=slvr.stokes_dtype)
+            stokes[:,:,0] = 1
+            stokes[:,:,1] = 0
+            stokes[:,:,2] = 0
+            stokes[:,:,3] = 0
+            slvr.transfer_stokes(stokes)
+            slvr.transfer_alpha(np.zeros(shape=slvr.alpha_shape, dtype=slvr.alpha_dtype))
+    
         # Solve the RIME
         slvr.solve()
 
