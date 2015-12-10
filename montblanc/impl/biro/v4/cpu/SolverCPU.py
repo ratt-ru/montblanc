Correct phase calculation in v4.
In v4, the phase Was previously calculated using 2*pi*1j.
It is now calculated with -2*pi*1j, which agrees with the RIME,
and with this commit, the v2 and v4 visibilities are the same.

However, v2 is still calculated with 2*pi*1j.

montblanc/impl/biro/v4/cpu/SolverCPU.py
@@ -164,7 +164,7 @@ class SolverCPU(object):
 
             # e^(2*pi*sqrt(u*l+v*m+w*n)*frequency/C).
             # Dim. ntime x na x nchan x nsrcs
-            cplx_phase = ne.evaluate('exp(2*pi*1j*p*f/C)', {
+            cplx_phase = ne.evaluate('exp(-2*pi*1j*p*f/C)', {
                 'p': phase[:, :, :, np.newaxis],
                 'f': freq[np.newaxis, np.newaxis, np.newaxis, :],
                 'C': montblanc.constants.C,