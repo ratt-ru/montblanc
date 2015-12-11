e9fd85093c26f656557d1ed36b0187ee78fbba24
Simon Perkins <simon.perkins@gmail.com>
2015-12-11 17:02:25 +0200

    MS_test.py now handles both v2 and v4.
    Useful for comparing implementations.

--- montblanc/examples/MS_test.py
+++ montblanc/examples/MS_test.py
@@ -21,40 +21,71 @@
 import numpy as np
 import montblanc
 
+from montblanc.config import (BiroSolverConfiguration,
+    BiroSolverConfigurationOptions as Options)
+
 if __name__ == '__main__':
     import sys
     import argparse
 
     parser = argparse.ArgumentParser(description='RIME MS test script')
     parser.add_argument('msfile', help='Measurement Set File')
-    parser.add_argument('-v','--version',dest='version', type=str, default='v2', choices=['v2','v3'],
+    parser.add_argument('-v','--version',dest='version', type=str, default='v2', choices=['v2','v3', 'v4'],
         help='BIRO Pipeline Version.')
 
     args = parser.parse_args(sys.argv[1:])
 
     # Get the solver.
-
     slvr_cfg = montblanc.rime_solver_cfg(msfile=args.msfile,
         sources=montblanc.sources(point=1, gaussian=0, sersic=0),
         dtype='double', version=args.version)
 
     with montblanc.rime_solver(slvr_cfg) as slvr:
-        # Create point sources at zeros
-        l=slvr.ft(np.ones(slvr.nsrc))*0.1
-        m=slvr.ft(np.ones(slvr.nsrc))*0.25
-        lm=np.array([l,m], dtype=slvr.ft).reshape(2,slvr.nsrc)
+        if args.version in [Options.VERSION_TWO, Options.VERSION_THREE]:
+            lm = np.empty(shape=slvr.lm_shape, dtype=slvr.lm_dtype)
+            l, m = lm[0,:], lm[1,:]
+            l[:] = 0.1
+            m[:] = 0.25
 
-        # Create 1Jy point sources
-        fI=slvr.ft(np.ones(slvr.ntime*slvr.nsrc)).reshape(slvr.ntime,slvr.nsrc)*2
-        fQ=slvr.ft(np.zeros(slvr.ntime*slvr.nsrc)).reshape(slvr.ntime,slvr.nsrc)
-        fU=slvr.ft(np.zeros(slvr.ntime*slvr.nsrc)).reshape(slvr.ntime,slvr.nsrc)
-        fV=slvr.ft(np.zeros(slvr.ntime*slvr.nsrc)).reshape(slvr.ntime,slvr.nsrc)
-        alpha=slvr.ft(np.ones(slvr.ntime*slvr.nsrc)).reshape(slvr.ntime,slvr.nsrc)*0.5
-        brightness = np.array([fI,fQ,fU,fV,alpha], dtype=slvr.ft)
+            slvr.transfer_lm(lm)
 
-        slvr.transfer_lm(lm)
-        slvr.transfer_brightness(brightness)
+            B = np.empty(shape=slvr.brightness_shape, dtype=slvr.brightness_dtype)
+            I, Q, U, V, alpha = B[0,:,:], B[1,:,:], B[2,:,:], B[3,:,:], B[4,:,:]
+            I[:] = 2
+            Q[:] = 1
+            U[:] = 1
+            V[:] = 1
+            alpha[:] = 0.5
+
+            slvr.transfer_brightness(B)
+        elif args.version in [Options.VERSION_FOUR, Options.VERSION_FIVE]:
+            lm = np.empty(shape=slvr.lm_shape, dtype=slvr.lm_dtype)
+            l, m = lm[:,0], lm[:,1]
+            l[:] = 0.1
+            m[:] = 0.25
+
+            slvr.transfer_lm(lm)
+
+            stokes = np.empty(shape=slvr.stokes_shape, dtype=slvr.stokes_dtype)
+            alpha = np.empty(shape=slvr.alpha_shape, dtype=slvr.alpha_dtype)
+            I, Q, U, V = stokes[:,:,0], stokes[:,:,1], stokes[:,:,2], stokes[:,:,3]
+            I[:] = 2
+            Q[:] = 1
+            U[:] = 1
+            V[:] = 1
+            alpha[:] = 0.5
+            slvr.transfer_stokes(stokes)
+            slvr.transfer_alpha(alpha)
 
         slvr.solve()
+
+        with slvr.context:
+            if args.version in [Options.VERSION_TWO, Options.VERSION_THREE]:
+                vis = slvr.vis_gpu.get().transpose(1, 2, 3, 0)
+            elif args.version in [Options.VERSION_FOUR, Options.VERSION_FIVE]:
+                vis = slvr.vis_gpu.get()
+
+            print vis
+            print vis.shape
 
         print slvr.X2
