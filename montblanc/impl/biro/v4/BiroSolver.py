a75832628693f60461a1812b559d83210c74549e
Simon Perkins <simon.perkins@gmail.com>
2016-03-29 13:52:36 +0200

    Separate antenna pair code from solver class.
    Manually graft antenna pair code onto the class
    during construction. This allows us to graft
    the same code onto the CPU solver in a future commit.

--- montblanc/impl/biro/v4/BiroSolver.py
+++ montblanc/impl/biro/v4/BiroSolver.py
@@ -72,6 +72,22 @@ class BiroSolver(CUDASolver):
             slvr_cfg[Options.E_BEAM_DEPTH],
             description='E Beam cube height in nu coords')
 
+        # Monkey patch these functions onto the object
+        # TODO: Remove this when deprecating v2.
+        import types
+        from ant_pairs import (get_default_base_ant_pairs,
+            get_default_ant_pairs,
+            get_ap_idx)
+
+        self.get_default_base_ant_pairs = types.MethodType(
+            get_default_base_ant_pairs, self)
+
+        self.get_default_ant_pairs = types.MethodType(
+            get_default_ant_pairs, self)
+
+        self.get_ap_idx = types.MethodType(
+            get_ap_idx, self)        
+
         from montblanc.impl.biro.v4.config import (A, P)
 
         self.register_properties(P)
@@ -81,90 +97,3 @@ class BiroSolver(CUDASolver):
 
     def const_data(self):
         return self._const_data
-
-    def get_default_base_ant_pairs(self):
-        """
-        Return an np.array(shape=(2, nbl), dtype=np.int32]) containing the
-        default antenna pairs for each baseline.
-        """
-        na = self.dim_local_size('na')
-        return np.int32(np.triu_indices(na, 1))
-
-    def get_default_ant_pairs(self):
-        """
-        Return an np.array(shape=(2, ntime, nbl), dtype=np.int32])
-        containing the default antenna pairs for each timestep
-        at each baseline.
-        """
-
-        # Create the antenna pair mapping, from upper triangle indices
-        # based on the number of antenna.
-        ntime, nbl = self.dim_local_size('ntime', 'nbl')
-        return np.tile(self.get_default_base_ant_pairs(), ntime) \
-            .reshape(2, ntime, nbl)
-
-    def get_ap_idx(self, default_ap=None, src=False, chan=False):
-        """
-        This method produces an index
-        which arranges per antenna values into a
-        per baseline configuration, using the supplied (default_ap)
-        per timestep and baseline antenna pair configuration.
-        Thus, indexing an array with shape (na) will produce
-        a view of the values in this array with shape (2, nbl).
-
-        Consequently, this method is suitable for indexing
-        an array of shape (ntime, na). Specifiying source
-        and channel dimensions allows indexing of an array
-        of shape (nsrc, ntime, na, nchan).
-
-        Using this index on an array of (ntime, na)
-        produces a (2, ntime, nbl) array,
-        or (2, nsrc, ntime, nbl, nchan) if source
-        and channel are also included.
-
-        The values for the first antenna are in position 0, while
-        those for the second are in position 1.
-
-        >>> ap = slvr.get_ap_idx()
-        >>> u_ant = np.random.random(size=(ntime,na))
-        >>> u_bl = u_ant[ap][1] - u_ant[ap][0]
-        >>> assert u_bl.shape == (2, ntime, nbl)
-        """
-
-        if default_ap is None:
-            default_ap = self.get_default_base_ant_pairs()
-
-        slvr = self
-
-        newdim = lambda d: [np.newaxis for n in range(d)]
-
-        sed = (1 if src else 0)     # Extra source dimension
-        ced = (1 if chan else 0)    # Extra channel dimension
-        ned = sed + ced             # Nr of extra dimensions
-        all = slice(None, None, 1)  # all slice
-        idx = []                    # Index we're returning
-        nsrc, ntime, nchan = slvr.dim_local_size('nsrc', 'ntime', 'nchan')
-
-        # Create the source index, [np.newaxis,:,np.newaxis,np.newaxis] + [...]
-        if src is True:
-            src_slice = tuple(newdim(1) + [all] + newdim(2) + newdim(ced))
-            idx.append(np.arange(nsrc)[src_slice])
-
-        # Create the time index, [np.newaxis] + [...]  + [:,np.newaxis] + [...]
-        time_slice = tuple(newdim(1) + newdim(sed) +
-            [all, np.newaxis] + newdim(ced))
-        idx.append(np.arange(ntime)[time_slice])
-
-        # Create the antenna pair index, [:] + [...]  + [np.newaxis,:] + [...]
-        ap_slice = tuple([all] + newdim(sed) +
-            [np.newaxis, all] + newdim(ced))
-        idx.append(default_ap[ap_slice])
-
-        # Create the channel index,
-        # Create the antenna pair index, [np.newaxis] + [...]  + [np.newaxis,np.newaxis] + [:]
-        if chan is True:
-            chan_slice = tuple(newdim(1) + newdim(sed) +
-                [np.newaxis, np.newaxis] + [all])
-            idx.append(np.arange(nchan)[chan_slice])
-
-        return tuple(idx)
