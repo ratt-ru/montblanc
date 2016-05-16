20dadbef69d19c8962a5fae4355dfca8315772fc
Simon Perkins <simon.perkins@gmail.com>
2016-05-16 09:29:12 +0200

    Add a simple kernel for testing purposes

--- montblanc/tensorflow/rime_ops/phase_op.cu.cc
+++ montblanc/tensorflow/rime_ops/phase_op.cu.cc
@@ -1,13 +1,29 @@
 #if GOOGLE_CUDA
 
-#include "tensorflow/core/framework/register_types.h"
 #include "tensorflow/core/framework/tensor.h"
+#include "tensorflow/core/framework/device_base.h"
 
 #define EIGEN_USE_GPU
 
+namespace gpu = perftools::gputools;
+//namespace gcudacc = platforms::gpus::gcudacc;
+
+
 namespace tensorflow {
 
+//gpu::Platform * platform = gpu::MultiPlatformManager::PlatformWithName("cuda").ValueOrDie();
+
 typedef Eigen::GpuDevice GPUDevice;
+
+template <typename FT, typename CT>
+__global__ void rime_phase(FT * data, int N)
+{
+    int i = blockIdx.x*blockDim.x + threadIdx.x;
+    FT value = data[i];
+    value *= 10.0;
+    data[i] = value;
+}
+
 
 }
 
