ae120142b9ed2b8168134de7dba8a3af52487578
Simon Perkins <simon.perkins@gmail.com>
2016-05-18 18:47:10 +0200

    Add numpy implementation of B Sqrt

--- montblanc/tensorflow/rime_ops/test_b_sqrt.py
+++ montblanc/tensorflow/rime_ops/test_b_sqrt.py
@@ -22,6 +22,43 @@ def b_sqrt_op(stokes, alpha, frequency, ref_freq):
 
     return mod.rime_b_sqrt(stokes, alpha, frequency, ref_freq, CT=CT)
 
+def b_sqrt(stokes, alpha, frequency, ref_freq):
+    I = stokes[:,:,0]
+    Q = stokes[:,:,1]
+    U = stokes[:,:,2]
+    V = stokes[:,:,3]
+
+    # Compute the spectral index
+    freq_ratio = frequency[np.newaxis,np.newaxis,:]/np.asscalar(ref_freq)
+    power = np.power(freq_ratio, alpha[:,:,np.newaxis])
+
+    B = np.empty(shape=(nsrc, ntime, nchan, 4), dtype=ctype)
+    B[:,:,:,0] = (I+Q)[:,:,np.newaxis]*power
+    B[:,:,:,1] = (U+V*1j)[:,:,np.newaxis]*power
+    B[:,:,:,2] = (U-V*1j)[:,:,np.newaxis]*power
+    B[:,:,:,3] = (I-Q)[:,:,np.newaxis]*power
+
+    # Compute the trace and determinant. Need power**2
+    # for det since its composed of squares
+    trace = (2*I)[:,:,np.newaxis]*power
+    det = (I**2 - Q**2 - U**2 - V**2)[:,:,np.newaxis]*(power**2)
+
+    # Compute values for computing matrix square root
+    s = np.sqrt(det);
+    t = np.sqrt(trace + 2.0*s);
+
+    B_sqrt = B.copy()
+
+    # Add s to the diagonals
+    B_sqrt[:,:,:,0] += s
+    B_sqrt[:,:,:,3] += s
+
+    # Divide all matrix entries by t
+    t[t == 0.0] = 1.0;
+    B_sqrt /= t[:,:,:,np.newaxis]
+
+    return B, B_sqrt
+
 dtype, ctype = np.float32, np.complex64
 nsrc, ntime, na, nchan = 100, 50, 64, 128
 
@@ -34,7 +71,7 @@ noise = np.random.random(size=(nsrc, ntime))*0.1
 # Need I^2 = Q^2 + U^2 + V^2 + noise^2
 np_stokes[:,:,0] = np.sqrt(Q**2 + U**2 + V**2 + noise)
 
-np_alpha = np.random.random(size=(nsrc, ntime)).astype(dtype)*0.1
+np_alpha = np.random.random(size=(nsrc, ntime)).astype(dtype)*0.8
 np_frequency = np.linspace(1.3e9, 1.5e9, nchan, endpoint=True, dtype=dtype)
 np_ref_freq = np.array([1.4e9], dtype=dtype)
 
@@ -65,6 +102,10 @@ with tf.Session() as S:
     tf_b_sqrt_op_cpu = S.run(b_sqrt_op_cpu)
     print 'Tensorflow CPU time %f' % (timeit.default_timer() - start)
 
+    start = timeit.default_timer()
+    np_b, np_b_sqrt = b_sqrt(np_stokes, np_alpha, np_frequency, np_ref_freq)
+    print 'Numpy CPU time %f' % (timeit.default_timer() - start)
+
     # Evaluate and time tensorflow GPU
     #start = timeit.default_timer()
     #tf_b_sqrt_op_gpu = S.run(b_sqrt_op_gpu)
@@ -72,5 +113,23 @@ with tf.Session() as S:
 
     # Check that our shapes and values agree with a certain tolerance
     assert tf_b_sqrt_op_cpu.shape == (nsrc, ntime, nchan, 4)
+    assert np_b_sqrt.shape == (nsrc, ntime, nchan, 4)
+    assert np.allclose(tf_b_sqrt_op_cpu, np_b_sqrt)
+
+    # Check that multiplying the matrix
+    np_b_flat = np_b.reshape(-1,2,2)
+    np_b_sqrt_flat = np_b_sqrt.reshape(-1,2,2)
+
+    # Check that multiplying the square root matrix
+    # by it's hermitian transpose yields the original
+    assert np.allclose(np_b_flat,
+        np.einsum("...ij,...kj->...ik",
+            np_b_sqrt_flat, np_b_sqrt_flat.conj()))
+    
+    # Check that multiplying the square root matrix
+    # by itself yields the original
+    assert np.allclose(np_b_flat,
+        np.einsum("...ij,...jk->...ik",
+            np_b_sqrt_flat, np_b_sqrt_flat))
 
 print 'Tests Succeeded'
