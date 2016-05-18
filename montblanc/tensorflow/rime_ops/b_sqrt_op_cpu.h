6bc6cba27ddc16daad6c6dffd9c4c07b0d28c607
Simon Perkins <simon.perkins@gmail.com>
2016-05-18 16:42:18 +0200

    Sanity check b sqrt op inputs, allocate output

--- montblanc/tensorflow/rime_ops/b_sqrt_op_cpu.h
+++ montblanc/tensorflow/rime_ops/b_sqrt_op_cpu.h
@@ -23,6 +23,47 @@ public:
     void Compute(tensorflow::OpKernelContext * context) override
     {
         namespace tf = tensorflow;
+
+        // Sanity check the input tensors
+        const tf::Tensor & in_stokes = context->input(0);
+        const tf::Tensor & in_alpha = context->input(1);
+        const tf::Tensor & in_frequency = context->input(2);
+        const tf::Tensor & in_ref_freq = context->input(3);
+
+        OP_REQUIRES(context, in_stokes.dims() == 3 && in_stokes.dim_size(2) == 4,
+            tf::errors::InvalidArgument(
+                "stokes should be of shape (nsrc, ntime, 4)"))
+
+        OP_REQUIRES(context, in_alpha.dims() == 2,
+            tf::errors::InvalidArgument(
+                "alpha should be of shape (nsrc, ntime)"))
+
+        OP_REQUIRES(context, in_frequency.dims() == 1,
+            tf::errors::InvalidArgument(
+                "frequency should be of shape (nchan)"))
+
+        OP_REQUIRES(context, in_ref_freq.dims() == 1 && in_ref_freq.dim_size(0) == 1,
+            tf::errors::InvalidArgument(
+                "ref_freq should be a scalar"))
+
+
+        // Extract problem dimensions
+        int ntime = in_stokes.dim_size(0);
+        int nsrc = in_stokes.dim_size(1);
+        int nchan = in_frequency.dim_size(0);
+
+        // Reason about our output shape
+        tf::TensorShape b_sqrt_shape({nsrc, ntime, nchan, 4});
+
+        // Create a pointer for the b_sqrt result
+        tf::Tensor * b_sqrt_ptr = nullptr;
+
+        // Allocate memory for the b_sqrt
+        OP_REQUIRES_OK(context, context->allocate_output(
+            0, b_sqrt_shape, &b_sqrt_ptr));
+
+        if (b_sqrt_ptr->NumElements() == 0)
+            { return; }        
     }
 };
 
