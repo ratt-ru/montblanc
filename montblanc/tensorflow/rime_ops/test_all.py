56a175269988b9dfb064bcab52c901227a58d309
Simon Perkins <simon.perkins@gmail.com>
2016-06-13 17:24:28 +0200

    Compute RIME over source type.
    Also use control dependencies to order kernel execution.

--- montblanc/tensorflow/rime_ops/test_all.py
+++ montblanc/tensorflow/rime_ops/test_all.py
@@ -9,16 +9,20 @@ rime = tf.load_op_library('rime.so')
 dtype = np.float64
 np_apply_dies = np.bool(True)
 
+# Infer our complex type
 ctype = np.complex64 if dtype == np.float32 else np.complex128
+
+# Lambdas for creating random floats and complex numbers
 rf = lambda *s: np.random.random(size=s).astype(dtype)
 rc = lambda *s: rf(*s) + rf(*s)*1j
 
-ntime, na, nchan = 15, 7, 16
+# Problem dimensions
+ntime, na, nchan = 50, 64, 64
 src_counts = npsrc, ngsrc, nssrc = 5, 6, 7
 nsrc = sum(src_counts)
 nbl = na*(na-1)//2
 npol = 4
-beam_lw = beam_mh = beam_nud = 50
+beam_lw = beam_mh = beam_nud = 100
 
 D = AttrDict()
 
@@ -63,67 +67,62 @@ D.sersic_param = rf(3, nssrc)*dtype([1,1,np.pi/648000])[:,np.newaxis]
 # Create tensorflow variables from numpy arrays
 args = AttrDict((n,tf.Variable(v, name=n)) for n, v in D.iteritems())
 
-# Compute the complex phase
-cplx_phase = rime.phase(args.lm, args.uvw,
-    args.frequency, CT=ctype)
+# Create some model visibilities
+model_vis = tf.Variable(rc(ntime, nbl, nchan, npol), name='model_vis')
 
-# Compute the brightness square root
-bsqrt = rime.b_sqrt(args.stokes, args.alpha, args.frequency,
-    args.ref_freq, CT=ctype)
+shape_map = [
+    lambda: tf.ones((npsrc, ntime, na, nchan), dtype=dtype),
+    lambda: rime.gauss_shape(args.uvw, args.ant1, args.ant2,
+        args.frequency, args.gauss_param),
+    lambda: rime.sersic_shape(args.uvw, args.ant1, args.ant2,
+        args.frequency, args.sersic_param),
+]
 
-# Compute the ejones from the beam cube
-ejones = rime.e_beam(args.lm, args.point_errors,
-    args.antenna_scaling, args.ebeam, args.parallactic_angle,
-    args.beam_ll, args.beam_lm,
-    args.beam_ul, args.beam_um)
-    
-# Compute per antenna jones terms    
-ant_jones = rime.ekb_sqrt(cplx_phase, bsqrt, ejones, FT=dtype)
+base = 0
+for scount, shape_fn in zip(src_counts, shape_map):
+    # Slice our source arrays
+    with tf.control_dependencies([model_vis]):
+        lm =     tf.slice(args.lm,     (base, 0   ), (scount, -1    ))
+        stokes = tf.slice(args.stokes, (base, 0, 0), (scount, -1, -1))
+        alpha =  tf.slice(args.alpha,  (base, 0   ), (scount, -1    ))
 
-# Compute the gaussian shape
-gauss_shape = rime.gauss_shape(args.uvw, args.ant1, args.ant2,
-    args.frequency, args.gauss_param)
+    # Compute the complex phase
+    with tf.control_dependencies([lm, stokes, alpha]):
+        cplx_phase = rime.phase(lm, args.uvw,
+            args.frequency, CT=ctype)
 
-# Compute the sersic shape
-sersic_shape = rime.sersic_shape(args.uvw, args.ant1, args.ant2,
-    args.frequency, args.sersic_param)
+        # Compute the brightness square root
+    with tf.control_dependencies([cplx_phase]):
+        bsqrt = rime.b_sqrt(stokes, alpha, args.frequency,
+            args.ref_freq, CT=ctype)
 
-# Zero the model visibilities
-model_vis = tf.zeros((ntime, nbl, nchan, npol), dtype=ctype)
+    # Compute the ejones from the beam cube
+    with tf.control_dependencies([bsqrt]):
+        ejones = rime.e_beam(lm, args.point_errors,
+            args.antenna_scaling, args.ebeam, args.parallactic_angle,
+            args.beam_ll, args.beam_lm,
+            args.beam_ul, args.beam_um)
+        
+    # Compute per antenna jones terms    
+    with tf.control_dependencies([ejones]):
+        ant_jones = rime.ekb_sqrt(cplx_phase, bsqrt, ejones, FT=dtype)
 
-# Pointing source shapes are one
-point_shape = tf.ones((npsrc, ntime, na, nchan), dtype=dtype)
+    # Sum coherencies over this source type
+    with tf.control_dependencies([ant_jones]):
+        model_vis = rime.sum_coherencies(args.ant1, args.ant2, shape_fn(),
+                ant_jones, args.flag, args.gterm, model_vis, False)
 
-# Slice the antenna jones array, referencing the point sources
-point_ant_jones = tf.slice(ant_jones,
-    (0,0,0,0,0), (npsrc,-1,-1,-1,-1))
-
-# Sum coherencies over point sources
-model_vis = rime.sum_coherencies(args.ant1, args.ant2, point_shape,
-        point_ant_jones, args.flag, args.gterm, model_vis, False)
-
-# Slice the antenna jones array, referencing the gaussian sources
-gauss_ant_jones = tf.slice(ant_jones,
-    (npsrc,0,0,0,0), (ngsrc,-1,-1,-1,-1))
-
-# Sum coherencies over gaussian sources
-model_vis = rime.sum_coherencies(args.ant1, args.ant2, gauss_shape,
-        gauss_ant_jones, args.flag, args.gterm, model_vis, False)
-
-# Slice the antenna jones array, referencing the sersic sources
-sersic_ant_jones = tf.slice(ant_jones,
-    (npsrc+ngsrc,0,0,0,0), (nssrc,-1,-1,-1,-1))
-
-# Sum coherencies over sersic sources
-model_vis = rime.sum_coherencies(args.ant1, args.ant2, sersic_shape,
-        sersic_ant_jones, args.flag, args.gterm, model_vis, True)
+    # Increase base offset by source count
+    base += scount
 
 with tf.Session() as S:
     run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+    #run_options = tf.RunOptions()
     run_metadata = tf.RunMetadata()
     S.run(tf.initialize_all_variables())
     
-    vis = S.run(model_vis, options=run_options, run_metadata=run_metadata)
+    for _ in range(10):
+        vis = S.run(model_vis, options=run_options, run_metadata=run_metadata)
 
     print vis.shape
 
