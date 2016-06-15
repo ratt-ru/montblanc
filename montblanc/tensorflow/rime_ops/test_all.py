1896437f05b3e7ae222f532427599c0fcd3aa277
Simon Perkins <simon.perkins@gmail.com>
2016-06-15 17:42:55 +0200

    Iterate over chunk sizes with python
    Don't use tf.while_loop, just submit loop
    bits with python and numpy.

--- montblanc/tensorflow/rime_ops/test_all.py
+++ montblanc/tensorflow/rime_ops/test_all.py
@@ -21,6 +21,7 @@ ntime, na, nchan = 25, 64, 64
 src_counts = npsrc, ngsrc, nssrc = 5, 6, 7
 src_types = POINT, GAUSS, SERSIC = 'point', 'gauss', 'sersic'
 nsrc = sum(src_counts)
+src_chunk = 5
 nbl = na*(na-1)//2
 npol = 4
 beam_lw = beam_mh = beam_nud = 100
@@ -61,81 +62,92 @@ def get_src_vars(nsrc, src_type=None):
 
     # lm coordinates
     lm = (rf(nsrc, 2) - 0.5)*0.1
-    lm = tf.Variable(lm, name='lm_{t}'.format(t=src_type))
 
     # Stokes parameters
     # Need I**2 = Q**2 + U**2 + V**2 + noise**2
     stokes = rf(nsrc, ntime, npol)
     Q, U, V = stokes[:,:,1], stokes[:,:,2], stokes[:,:,3]
     stokes[:,:,0] = Q**2 + U**2 + V**2 + (rf(nsrc, ntime)*0.1)**2
-    stokes = tf.Variable(stokes, name='stokes_{t}'.format(t=src_type))
 
     # Alpha
     alpha = rf(nsrc, ntime)*0.8
-    alpha = tf.Variable(alpha, name='alpha_{t}'.format(t=src_type))
 
     # Shape parameters
     if src_type == POINT:
-        shape = tf.ones((nsrc, ntime, nbl, nchan),
-            dtype=dtype, name='{t}_shape'.format(t=POINT))
+        param = None
     elif src_type == GAUSS:
         # Shape parameters
         gauss_param = rf(3, nsrc)*dtype([0.1,0.1,1])[:,np.newaxis]
-        gauss_param = tf.Variable(gauss_param, name='gauss_param')
-
-        shape = rime.gauss_shape(args.uvw, args.ant1, args.ant2,
-            args.frequency, gauss_param,
-            name='{t}_shape'.format(t=GAUSS))
+        param = gauss_param
     elif src_type == SERSIC:
         sersic_param = rf(3, nsrc)*dtype([1,1,np.pi/648000])[:,np.newaxis]
-        sersic_param = tf.Variable(sersic_param, name='sersic_param')
-
-        shape = rime.sersic_shape(args.uvw, args.ant1, args.ant2,
-            args.frequency, sersic_param,
-            name='{t}_shape'.format(t=SERSIC))
+        param = sersic_param
     else:
         raise ValueError("Unknown source type {t}.".format(t=src_type))
 
-    return lm, stokes, alpha, shape
-
+    return lm, stokes, alpha, param
 
 # Create some model visibilities
 model_vis = tf.Variable(rc(ntime, nbl, nchan, npol), name='model_vis')
 
 base = 0
-for scount, stype in zip(src_counts, src_types):
-    # Slice our source arrays
-    with tf.control_dependencies([model_vis]):
-        lm, stokes, alpha, shape = get_src_vars(scount, stype)
+for src_count, src_type in zip(src_counts, src_types):
+    # Generate some source arrays for this source type
+    np_lm, np_stokes, np_alpha, np_param = get_src_vars(src_count, src_type)
 
-    # Compute the complex phase
-    with tf.control_dependencies([lm, stokes, alpha]):
-        cplx_phase = rime.phase(lm, args.uvw,
-            args.frequency, CT=ctype)
+    # Loop to src_count in src_chunk chunks
+    for i in range(0, src_count, src_chunk):
+        chunk_end = min(i+src_chunk, src_count)
 
-        # Compute the brightness square root
-    with tf.control_dependencies([cplx_phase]):
-        bsqrt = rime.b_sqrt(stokes, alpha, args.frequency,
-            args.ref_freq, CT=ctype)
+        # Create tensorflow variables from nump array slices
+        lm = tf.Variable(np_lm[i:chunk_end,:])
+        stokes = tf.Variable(np_stokes[i:chunk_end])
+        alpha = tf.Variable(np_alpha[i:chunk_end])
 
-    # Compute the ejones from the beam cube
-    with tf.control_dependencies([bsqrt]):
-        ejones = rime.e_beam(lm, args.point_errors,
-            args.antenna_scaling, args.ebeam, args.parallactic_angle,
-            args.beam_ll, args.beam_lm,
-            args.beam_ul, args.beam_um)
-        
-    # Compute per antenna jones terms    
-    with tf.control_dependencies([ejones]):
-        ant_jones = rime.ekb_sqrt(cplx_phase, bsqrt, ejones, FT=dtype)
+        # Compute the complex phase
+        with tf.control_dependencies([lm, stokes, alpha]):
+            cplx_phase = rime.phase(lm, args.uvw,
+                args.frequency, CT=ctype)
 
-    # Sum coherencies over this source type
-    with tf.control_dependencies([ant_jones]):
-        model_vis = rime.sum_coherencies(args.ant1, args.ant2, shape,
-                ant_jones, args.flag, args.gterm, model_vis, False)
+            # Compute the brightness square root
+        with tf.control_dependencies([cplx_phase]):
+            bsqrt = rime.b_sqrt(stokes, alpha, args.frequency,
+                args.ref_freq, CT=ctype)
+
+        # Compute the ejones from the beam cube
+        with tf.control_dependencies([bsqrt]):
+            ejones = rime.e_beam(lm, args.point_errors,
+                args.antenna_scaling, args.ebeam, args.parallactic_angle,
+                args.beam_ll, args.beam_lm,
+                args.beam_ul, args.beam_um)
+            
+        # Compute per antenna jones terms    
+        with tf.control_dependencies([ejones]):
+            ant_jones = rime.ekb_sqrt(cplx_phase, bsqrt, ejones, FT=dtype)
+
+        # Compute shape parameters
+        with tf.control_dependencies([ant_jones]):
+            if src_type == POINT:
+                shape = tf.ones([chunk_end-i, ntime, nbl, nchan], dtype=dtype)
+            elif src_type == GAUSS:
+                gauss_param = tf.Variable(np_param[:,i:chunk_end])
+                shape = rime.gauss_shape(args.uvw, args.ant1, args.ant2,
+                    args.frequency, gauss_param,
+                    name='{t}_shape'.format(t=GAUSS))
+            elif src_type == SERSIC:
+                sersic_param = tf.Variable(np_param[:,i:chunk_end])
+                shape = rime.sersic_shape(args.uvw, args.ant1, args.ant2,
+                    args.frequency, sersic_param,
+                    name='{t}_shape'.format(t=SERSIC))
+
+
+        # Sum coherencies over this source type
+        with tf.control_dependencies([shape]):
+            model_vis = rime.sum_coherencies(args.ant1, args.ant2, shape,
+                    ant_jones, args.flag, args.gterm, model_vis, False)
 
     # Increase base offset by source count
-    base += scount
+    base += src_count
 
 with tf.Session() as S:
     run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
