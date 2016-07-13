e43dd6b92f16d0def72f70c70a17543636b3340f
Simon Perkins <simon.perkins@gmail.com>
2016-07-13 17:59:33 +0200

    Simplifiy defaults/supplied dicts into one
    Called data_sources.

--- montblanc/impl/rime/tensorflow/feeders/queue_wrapper.py
+++ montblanc/impl/rime/tensorflow/feeders/queue_wrapper.py
@@ -3,58 +3,26 @@ import tensorflow as tf
 import pyrap.tables as pt
 import types
 
-def _get_queue_types(fed_arrays, defaults, supplied):
+def _get_queue_types(fed_arrays, data_sources):
     """
     Given a dictionaries of supplied and default arrays,
     return a list of types associated with each array in fed_arrays.
     """
 
-    def _get_queue_type(array_data):
-        """ Return a list of types for each array in fed_array """
-
-        head, tail = ((array_data[0], array_data[1:])
-            if isinstance(array_data, (tuple, list))
-            else (array_data, None) )
-
-        # Supplied with some type in the head
-        if isinstance(head, type):
-            return head
-        # CASA Measurement Set, get the type
-        # of the given column name in the tail
-        if isinstance(head, pt.table):
-            column = tail[0]
-            col_desc = head.getcoldesc(column)
-            return MS_TO_NP_TYPE_MAP[col_desc['valueType']]
-        # Tensorflow tensor
-        elif isinstance(head, tf.python.framework.ops.Tensor):
-            return head.dtype
-        # Numpy array
-        elif isinstance(head, np.ndarray):
-            return head.dtype
-        # Functor
-        elif isinstance(head, (types.LambdaType, types.MethodType)):
-            return tail[0]
-        else:
-            raise ValueError("Unhandled queue type {qt}".format(qt=type(head)))
-
     # Preferably use supplied data else take from defaults
     try:
-        array_data = (supplied.get(n) if n in supplied
-            else defaults.get(n) for n in fed_arrays)
-        return [_get_queue_type(a) for a in array_data]
+        return [data_sources.get(n)[1] for n in fed_arrays]
     except KeyError as e:
         raise ValueError("Array '{k}' was not provided in either "
             "the 'supplied' or 'defaults' arrays".format(k=e.message))
 
-
 class QueueWrapper(object):
-    def __init__(self, queue_size, fed_arrays, defaults, supplied):
-        self._defaults = defaults
+    def __init__(self, queue_size, fed_arrays, data_sources):
         self._fed_arrays = fed_arrays
-        self._supplied = supplied
+        self._data_sources = data_sources
 
         # Infer types of the given fed_arrays
-        self._queue_types = _get_queue_types(fed_arrays, defaults, supplied)
+        self._queue_types = _get_queue_types(fed_arrays, data_sources)
 
         # Create placeholders for the fed arrays
         self._placeholders = [tf.placeholder(dt, name="{n}_placeholder".format(n=n))
@@ -88,37 +56,13 @@ class QueueWrapper(object):
             s=len(self._queue_types),
             t=self._queue_types)
 
-def create_queue_wrapper(queue_size, fed_arrays, defaults, supplied):
+def create_queue_wrapper(queue_size, fed_arrays, data_sources):
     """
     Arguments
         fed_arrays: list
             array names that will be fed by this queue
-        supplied: dict
-            numpy arrays/functions/MStables used to feed arrays named
-            in fed_arrays
-        defaults:
-            defaults taken from this dictionary if necessary
+        data_sources: dict
+            (lambda/method, dtype) tuples, keyed on array names
 
     """
-    return QueueWrapper(queue_size, fed_arrays, defaults, supplied)
-
-"""
-fed_arrays = ['observed_vis', 'alpha', 'stokes', 'lm', 'flag', 'weight']
-
-ms = pt.table('/home/sperkins/data/WSRT.MS', ack=False)
-
-defaults = {
-    'alpha' : (lambda x: np.zeros(1000), np.float32),
-    'flag' : tf.Variable(tf.zeros((1000,), dtype=tf.uint8)),
-    'weight' : (ms, 'WEIGHT'),
-}
-
-supplied = {
-    'observed_vis': np.empty((10,10)),
-    'stokes': np.empty((10,10)),
-    'lm' : np.empty((100,100))
-}
-
-print create_queue_wrapper(10, fed_arrays, defaults,  supplied)
-ms.close()
-"""
+    return QueueWrapper(queue_size, fed_arrays, data_sources)
