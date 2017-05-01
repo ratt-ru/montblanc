9c1cd80b5d1eae70f6a2880b92b90440d25ebe1f
Simon Perkins <simon.perkins@gmail.com>
2017-05-01 17:51:09 +0200

    Try measures server per thread

--- montblanc/util/parallactic_angles.py
+++ montblanc/util/parallactic_angles.py
@@ -22,37 +22,7 @@ import threading
 
 import numpy as np
 
-class MeasuresServerWrapper(object):
-    """
-    Thread-safe Context Manager for a measures server object
-
-    .. code-block:: python
-
-        server = MeasuresServerWrapper()
-
-        with server as pm:
-            zenith = pm.direction('AZEL','0deg','90deg')
-    """
-    def __init__(self):
-        self._lock = threading.Lock()
-
-    def __enter__(self):
-        self._lock.acquire()
-
-        try:
-            # Already created, re-use
-            return self._measure
-        except AttributeError:
-            # Create
-            import pyrap.measures
-            self._measure = pyrap.measures.measures()
-
-        return self._measure
-
-    def __exit__(self, evalue, etype, etraceback):
-        self._lock.release()
-
-_measures_server = MeasuresServerWrapper()
+thread_local = threading.local()
 
 def parallactic_angles(times, antenna_positions, field_centre):
     """
@@ -73,29 +43,34 @@ def parallactic_angles(times, antenna_positions, field_centre):
         An array of parallactic angles per time-step
 
     """
+    import pyrap.measures
     import pyrap.quanta as pq
 
-    with _measures_server as pm:
-        # Create direction measure for the zenith
-        zenith = pm.direction('AZEL','0deg','90deg')
+    try:
+        pm = thread_local.pm
+    except AttributeError:
+        pm = thread_local.pm = pyrap.measures.measures()
 
-        # Create position measures for each antenna
-        reference_positions = [pm.position('itrf',
-            *(pq.quantity(x,'m') for x in pos))
-            for pos in antenna_positions]
+    # Create direction measure for the zenith
+    zenith = pm.direction('AZEL','0deg','90deg')
 
-        # Compute field centre in radians
-        fc_rad = pm.direction('J2000',
-            *(pq.quantity(f,'rad') for f in field_centre))
+    # Create position measures for each antenna
+    reference_positions = [pm.position('itrf',
+        *(pq.quantity(x,'m') for x in pos))
+        for pos in antenna_positions]
 
-        parallactic_angles = np.asarray([[
-                # Set antenna position as the reference frame
-                pm.do_frame(rp) and
-                # Set current time as the reference frame
-                pm.do_frame(pm.epoch("UTC",pq.quantity(t,"s"))) and
-                # Now compute the parallactic angle
-                pm.posangle(fc_rad, zenith).get_value("rad")]
-            for t in times
-            for rp in reference_positions])
+    # Compute field centre in radians
+    fc_rad = pm.direction('J2000',
+        *(pq.quantity(f,'rad') for f in field_centre))
 
-        return parallactic_angles
+    parallactic_angles = np.asarray([[
+            # Set antenna position as the reference frame
+            pm.do_frame(rp) and
+            # Set current time as the reference frame
+            pm.do_frame(pm.epoch("UTC",pq.quantity(t,"s"))) and
+            # Now compute the parallactic angle
+            pm.posangle(fc_rad, zenith).get_value("rad")]
+        for t in times
+        for rp in reference_positions])
+
+    return parallactic_angles
