3c6851cc4f572791067cbacfb1f5bc57e8d8f601
Simon Perkins <simon.perkins@gmail.com>
2017-05-01 17:47:10 +0200

    Try a thread-safe measures server

--- montblanc/util/parallactic_angles.py
+++ montblanc/util/parallactic_angles.py
@@ -18,7 +18,41 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
+import threading
+
 import numpy as np
+
+class MeasuresServerWrapper(object):
+    """
+    Thread-safe Context Manager for a measures server object
+
+    .. code-block:: python
+
+        server = MeasuresServerWrapper()
+
+        with server as pm:
+            zenith = pm.direction('AZEL','0deg','90deg')
+    """
+    def __init__(self):
+        self._lock = threading.Lock()
+
+    def __enter__(self):
+        self._lock.acquire()
+
+        try:
+            # Already created, re-use
+            return self._measure
+        except AttributeError:
+            # Create
+            import pyrap.measures
+            self._measure = pyrap.measures.measures()
+
+        return self._measure
+
+    def __exit__(self, evalue, etype, etraceback):
+        self._lock.release()
+
+_measures_server = MeasuresServerWrapper()
 
 def parallactic_angles(times, antenna_positions, field_centre):
     """
@@ -26,12 +60,12 @@ def parallactic_angles(times, antenna_positions, field_centre):
     reference antenna position and field centre.
 
     Arguments:
-        antenna_positions: ndarray of shape (na, 3)
-            Antenna positions, obtained from POSITION
-            column of MS ANTENNA sub-table
         times: ndarray
             Array of unique times with shape (ntime,),
             obtained from TIME column of MS table
+        antenna_positions: ndarray of shape (na, 3)
+            Antenna positions, obtained from POSITION
+            column of MS ANTENNA sub-table
         field_centre : ndarray of shape (2,)
             Field centre, should be obtained from MS PHASE_DIR
 
@@ -39,34 +73,29 @@ def parallactic_angles(times, antenna_positions, field_centre):
         An array of parallactic angles per time-step
 
     """
-    import pyrap.measures
     import pyrap.quanta as pq
 
-    pm = pyrap.measures.measures()
+    with _measures_server as pm:
+        # Create direction measure for the zenith
+        zenith = pm.direction('AZEL','0deg','90deg')
 
-    ntime = times.shape[0]
-    na = antenna_positions.shape[0]
+        # Create position measures for each antenna
+        reference_positions = [pm.position('itrf',
+            *(pq.quantity(x,'m') for x in pos))
+            for pos in antenna_positions]
 
-    # Create direction measure for the zenith
-    zenith = pm.direction('AZEL','0deg','90deg')
+        # Compute field centre in radians
+        fc_rad = pm.direction('J2000',
+            *(pq.quantity(f,'rad') for f in field_centre))
 
-    # Create position measures for each antenna
-    reference_positions = [pm.position('itrf',
-        *(pq.quantity(x,'m') for x in pos))
-        for pos in antenna_positions]
+        parallactic_angles = np.asarray([[
+                # Set antenna position as the reference frame
+                pm.do_frame(rp) and
+                # Set current time as the reference frame
+                pm.do_frame(pm.epoch("UTC",pq.quantity(t,"s"))) and
+                # Now compute the parallactic angle
+                pm.posangle(fc_rad, zenith).get_value("rad")]
+            for t in times
+            for rp in reference_positions])
 
-    # Compute field centre in radians
-    fc_rad = pm.direction('J2000',
-        *(pq.quantity(f,'rad') for f in field_centre))
-
-    parallactic_angles = np.asarray([[
-            # Set antenna position as the reference frame
-            pm.do_frame(rp) and
-            # Set current time as the reference frame
-            pm.do_frame(pm.epoch("UTC",pq.quantity(t,"s"))) and
-            # Now compute the parallactic angle
-            pm.posangle(fc_rad, zenith).get_value("rad")]
-        for t in times
-        for rp in reference_positions])
-
-    return parallactic_angles
+        return parallactic_angles
