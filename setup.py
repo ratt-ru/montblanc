20fb74f82cb543b41478c3c6e42293276e622b4a
Simon Perkins <simon.perkins@gmail.com>
2017-09-19 11:33:17 +0200

    Depend on cppimport and pybind11

--- setup.py
+++ setup.py
@@ -168,8 +168,10 @@ else:
     install_requires += [
         'astropy >= 1.3.0',
         'cerberus >= 1.1',
+        'cppimport >= 17.9.18',
         'numpy >= 1.11.3',
         'numexpr >= 2.6.1',
+        'pybind11 >= 2.2.0',
         'python-casacore >= 2.1.2',
         'ruamel.yaml >= 0.15.22',
         "{} == 1.4.0".format(tensorflow_package),
