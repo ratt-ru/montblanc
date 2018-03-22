f6583a113f4f50449b002ea4ecebb37a8149abfc
Simon Perkins <simon.perkins@gmail.com>
2018-03-22 15:31:17 +0200

    Rework QueueDataset
    Placing the Queue in the Dataset didn't work because
    _as_variant_tensor would create a new Dataset for both
    the iterator creation and enqueueing ops. As no common
    ancestor existed, no data would be available for the
    iterator to consume.
    
    Changed the workflow to create a QueueResource which,
    in turn, is consumed by the QueueDataset, then consumed
    by the Dataset iterator. Thus, QueueResource is the
    common ancestor for both the Dataset and the
    enqueuing operation.

--- montblanc/impl/rime/tensorflow/rime_ops/simple_queue_dataset.cpp
+++ montblanc/impl/rime/tensorflow/rime_ops/simple_queue_dataset.cpp
@@ -1,3 +1,5 @@
+#include <deque>
+
 #include "tensorflow/core/framework/dataset.h"
 #include "tensorflow/core/framework/common_shape_fns.h"
 #include "tensorflow/core/framework/partial_tensor_shape.h"
@@ -11,165 +13,325 @@ namespace montblanc {
 
 namespace {
 
-// See documentation in ../ops/dataset_ops.cc for a high-level
-// description of the following op.
-
 using namespace tensorflow;
 
-class QueuedTensorDatasetOp : public DatasetOpKernel {
+class QueueResource : public ResourceBase
+{
 private:
+    mutex mu_;
+
+    condition_variable cv_ GUARDED_BY(mu_);
+    std::deque<std::vector<Tensor>> entries_ GUARDED_BY(mu_);
+    bool closed_ GUARDED_BY(mu_);
+
     DataTypeVector dtypes_;
-    std::vector<TensorShape> shapes_;
+    std::vector<PartialTensorShape> shapes_;
+
+private:
+    bool _is_closed(void) EXCLUSIVE_LOCKS_REQUIRED(mu_)
+        { return closed_; }
 
 public:
-  explicit QueuedTensorDatasetOp(OpKernelConstruction* ctx)
-    : DatasetOpKernel(ctx)
+    explicit QueueResource(const DataTypeVector & dtypes,
+                           const std::vector<PartialTensorShape> & shapes)
+      : dtypes_(dtypes), shapes_(shapes), closed_(false)
     {
-      OP_REQUIRES_OK(ctx, ctx->GetAttr("Toutput_types", &dtypes_));
-      OP_REQUIRES_OK(ctx, ctx->GetAttr("Toutput_shapes", &shapes_));
     }
 
-  void MakeDataset(OpKernelContext* ctx, DatasetBase** output) override
+    void close(void) LOCKS_EXCLUDED(mu_)
+    {
+        mutex_lock l(mu_);
+        closed_ = true;
+    }
+
+    Status insert(std::vector<Tensor> tensors) LOCKS_EXCLUDED(mu_)
+    {
+        mutex_lock l(mu_);
+
+        if(_is_closed())
+            { return errors::OutOfRange("Queue is closed"); }
+
+        entries_.push_back(std::move(tensors));
+        cv_.notify_all();
+
+        return Status::OK();
+    }
+
+    Status pop(std::vector<Tensor> * out) LOCKS_EXCLUDED(mu_)
+    {
+        mutex_lock l(mu_);
+
+        if(entries_.empty() && _is_closed())
+            { return errors::OutOfRange("Queue is closed"); }
+
+        // Wait if empty
+        while(entries_.empty())
+            { cv_.wait(l); }
+
+        // Pop the first entry and return it
+        *out = std::move(entries_.front());
+        entries_.pop_front();
+
+        return Status::OK();
+    }
+
+    const DataTypeVector &
+    output_dtypes() const
+      { return dtypes_; }
+
+    const std::vector<PartialTensorShape> &
+    output_shapes() const
+      { return shapes_; }
+
+    string DebugString() override
+      { return "QueueResource"; }
+
+};
+
+class DatasetQueueHandleOp : public OpKernel
+{
+private:
+    mutex mu_;
+
+    DataTypeVector dtypes_;
+    std::vector<PartialTensorShape> shapes_;
+
+    ContainerInfo cinfo GUARDED_BY(mu_);
+    bool initialised GUARDED_BY(mu_);
+
+public:
+    explicit DatasetQueueHandleOp(OpKernelConstruction * ctx)
+                : OpKernel(ctx),
+                  initialised(false)
+    {
+        OP_REQUIRES_OK(ctx, ctx->GetAttr("Toutput_types", &dtypes_));
+        OP_REQUIRES_OK(ctx, ctx->GetAttr("Toutput_shapes", &shapes_));
+    }
+
+    ~DatasetQueueHandleOp() override
+    {
+        if(cinfo.resource_is_private_to_kernel())
+        {
+            if(!cinfo.resource_manager()->Delete<QueueResource>(
+                cinfo.container(), cinfo.name()).ok())
+            {
+              // Do nothing; the resource will have been deleted by session resets.
+            }
+        }
+    }
+
+    void Compute(OpKernelContext * ctx) override LOCKS_EXCLUDED(mu_)
+    {
+        mutex_lock l(mu_);
+
+        // If not initialised, get the resource manager
+        // and create the QueueResource
+        if(!initialised)
+        {
+            ResourceMgr * mgr = ctx->resource_manager();
+            OP_REQUIRES_OK(ctx, cinfo.Init(mgr, def()));
+
+            QueueResource * resource;
+            OP_REQUIRES_OK(ctx, mgr->LookupOrCreate<QueueResource>(
+                cinfo.container(), cinfo.name(), &resource,
+                [this, ctx](QueueResource ** result) EXCLUSIVE_LOCKS_REQUIRED(mu_)
+                {
+                    *result = new QueueResource(dtypes_, shapes_);
+                    return Status::OK();
+                }
+            ));
+
+            initialised = true;
+        }
+
+        // Now assign the QueueResource to output position 0
+        OP_REQUIRES_OK(ctx, MakeResourceHandleToOutput(
+                  ctx, 0, cinfo.container(), cinfo.name(),
+                  MakeTypeIndex<QueueResource>()));
+    }
+};
+
+REGISTER_OP("DatasetQueueHandle")
+    .Output("queue_handle: resource")
+    .Attr("Toutput_types: list(type) >= 1")
+    .Attr("Toutput_shapes: list(shape) >= 1")
+    .Attr("container: string = ''")
+    .Attr("shared_name: string = ''")
+    .SetIsStateful()  // Source dataset ops must be marked
+                      // stateful to inhibit constant folding.
+    .SetShapeFn(shape_inference::ScalarShape);
+
+REGISTER_KERNEL_BUILDER(Name("DatasetQueueHandle")
+                        .Device(DEVICE_CPU),
+                        DatasetQueueHandleOp);
+
+class DatasetQueueEnqueueOp : public OpKernel
+{
+private:
+    mutex mu_;
+
+public:
+    explicit DatasetQueueEnqueueOp(OpKernelConstruction * ctx) : OpKernel(ctx) {}
+
+    void Compute(OpKernelContext * ctx) override LOCKS_EXCLUDED(mu_)
+    {
+        mutex_lock l(mu_);
+
+        QueueResource * queue_resource;
+        OP_REQUIRES_OK(ctx, LookupResource(ctx, HandleFromInput(ctx, 0),
+                                          &queue_resource));
+
+        core::ScopedUnref unref_queue(queue_resource);
+
+        // Convert component Tensors into a vector
+        OpInputList components;
+        OP_REQUIRES_OK(ctx, ctx->input_list("components", &components));
+
+        std::vector<Tensor> tensors;
+        for (int c = 0; c < components.size(); ++c)
+            { tensors.emplace_back(std::move(components[c])); }
+
+        // Insert
+        OP_REQUIRES_OK(ctx, queue_resource->insert(std::move(tensors)));
+    }
+};
+
+REGISTER_OP("DatasetQueueEnqueue")
+    .Input("queue_handle: resource")
+    .Input("components: Toutput_types")
+    .Attr("Toutput_types: list(type) >= 1")
+    .Attr("container: string = ''")
+    .Attr("shared_name: string = ''")
+    .SetIsStateful()  // Source dataset ops must be marked
+                      // stateful to inhibit constant folding.
+    .SetShapeFn(shape_inference::NoOutputs);
+
+REGISTER_KERNEL_BUILDER(Name("DatasetQueueEnqueue")
+                        .Device(DEVICE_CPU),
+                        DatasetQueueEnqueueOp);
+
+
+class QueueCloseOp : public OpKernel
+{
+private:
+    mutex mu_;
+
+public:
+    explicit QueueCloseOp(OpKernelConstruction * ctx) : OpKernel(ctx) {}
+
+    void Compute(OpKernelContext * ctx) override LOCKS_EXCLUDED(mu_)
+    {
+        mutex_lock l(mu_);
+
+        // Obtain queue resource and close it
+        QueueResource * queue_resource;
+        OP_REQUIRES_OK(ctx, LookupResource(ctx, HandleFromInput(ctx, 0),
+                                          &queue_resource));
+
+        core::ScopedUnref unref_queue(queue_resource);
+
+        queue_resource->close();
+    }
+};
+
+REGISTER_OP("DatasetQueueClose")
+    .Input("queue_handle: resource")
+    .Attr("container: string = ''")
+    .Attr("shared_name: string = ''")
+    .SetIsStateful()  // Source dataset ops must be marked
+                      // stateful to inhibit constant folding.
+    .SetShapeFn(shape_inference::NoOutputs);
+
+REGISTER_KERNEL_BUILDER(Name("DatasetQueueClose")
+                        .Device(DEVICE_CPU),
+                        QueueCloseOp);
+
+
+// See documentation in ../ops/dataset_ops.cc for a high-level
+// description of the following op.
+class QueueDatasetOp : public DatasetOpKernel
+{
+public:
+    explicit QueueDatasetOp(OpKernelConstruction * ctx)
+                    : DatasetOpKernel(ctx) {}
+
+    void MakeDataset(OpKernelContext * ctx, DatasetBase ** output) override
+    {
+        QueueResource * queue_resource;
+        OP_REQUIRES_OK(ctx, LookupResource(ctx, HandleFromInput(ctx, 0),
+                                          &queue_resource));
+
+        core::ScopedUnref unref_queue(queue_resource);
+
+        *output = new Dataset(ctx, queue_resource);
+        // TODO(sjperkins)
+        // Sometimes this is needed if kind of nothing is associated
+        // with the dataset (iterators and next operators???????
+        //(*output)->Ref();
+    }
+
+private:
+  class Dataset : public GraphDatasetBase
   {
-    std::vector<PartialTensorShape> partial_shapes;
+  public:
+      QueueResource * queue_resource_;
 
-    for(int s=0; s < shapes_.size(); ++s)
-    {
-      PartialTensorShape partial_shape;
-      const auto & shape = shapes_[s];
-      const auto & dtype = dtypes_[s];
-      for(int r=0; r < shape.dims(); ++r)
+      explicit Dataset(OpKernelContext * ctx, QueueResource * queue_resource)
+          : GraphDatasetBase(ctx),
+            queue_resource_(queue_resource)
       {
-        partial_shape.AddDim(shape.dim_size(r));
+         queue_resource_->Ref();
       }
-      partial_shapes.emplace_back(partial_shape);
+
+    ~Dataset() override
+        { queue_resource_->Unref(); }
+
+    const DataTypeVector & output_dtypes() const override
+        { return queue_resource_->output_dtypes(); }
+
+    const std::vector<PartialTensorShape> & output_shapes() const override
+        { return queue_resource_->output_shapes(); }
+
+    string DebugString()
+        { return "QueueDataset"; }
+
+    std::unique_ptr<IteratorBase>
+    MakeIterator(const string & prefix) const override
+    {
+        return std::unique_ptr<IteratorBase>(new Iterator(
+          {this, strings::StrCat(prefix, "::QueueDataset")}));
     }
-
-    *output = new Dataset(ctx, dtypes_, partial_shapes);
-  }
-
- private:
-  class Dataset : public GraphDatasetBase {
-   private:
-    std::vector<PartialTensorShape> shapes;
-    DataTypeVector dtypes;
-
-   public:
-    Dataset(OpKernelContext* ctx,
-            const DataTypeVector & dtypes_,
-            const std::vector<PartialTensorShape> & shapes_)
-        : GraphDatasetBase(ctx), dtypes(dtypes_), shapes(shapes_) {}
-
-    std::unique_ptr<IteratorBase> MakeIterator(
-        const string& prefix) const override {
-      return std::unique_ptr<IteratorBase>(
-          new Iterator({this, strings::StrCat(prefix, "::SimpleQueue")}));
-    }
-
-    const DataTypeVector& output_dtypes() const override {
-      return dtypes;
-    }
-
-    const std::vector<PartialTensorShape>& output_shapes() const override {
-      return shapes;
-    }
-
-    string DebugString() override {
-      return strings::StrCat("QueuedTensorDatasetOp()::Dataset");
-    }
-
-   protected:
-    Status AsGraphDefInternal(DatasetGraphDefBuilder* b,
-                              Node** output) const override {
-
-      AttrValue output_types;
-      b->BuildAttrValue(dtypes, &output_types);
-      AttrValue output_shapes;
-      b->BuildAttrValue(shapes, &output_shapes);
-
-      TF_RETURN_IF_ERROR(b->AddDataset(this, {},
-                   {{"Toutput_types", output_types},
-                    {"output_shapes", output_shapes}},
-                   output));
+  };
 
 
-      return Status::OK();
-    }
+  class Iterator : public DatasetIterator<Dataset>
+  {
+  private:
 
-   private:
-    class Iterator : public DatasetIterator<Dataset> {
-     public:
-      explicit Iterator(const Params& params)
-          : DatasetIterator<Dataset>(params) {}
+  public:
+    explicit Iterator(const Params & params)
+        : DatasetIterator<Dataset>(params) {}
 
-      Status GetNextInternal(IteratorContext* ctx,
-                             std::vector<Tensor>* out_tensors,
-                             bool* end_of_sequence) override {
-        mutex_lock l(mu_);
-
-        const auto & shapes = dataset()->shapes;
-        const auto & dtypes = dataset()->dtypes;
-
-        std::vector<TensorShape> max_shapes;
-
-        for(int i = 0; i < dtypes.size(); ++i)
+        virtual Status GetNextInternal(IteratorContext * ctx,
+                    std::vector<Tensor> * out_tensors,
+                    bool * end_of_sequence) override
         {
-          const PartialTensorShape& shape = shapes[i];
-          TensorShape out_shape;
-
-          for (int d = 0; d < shape.dims(); ++d)
-          {
-            out_shape.AddDim(shape.dim_size(d));
-          }
-
-          max_shapes.push_back(std::move(out_shape));
+            *end_of_sequence = !dataset()->queue_resource_
+                                         ->pop(out_tensors).ok();
+            return Status::OK();
         }
-
-        for(int s=0; s < shapes.size(); ++s)
-        {
-          Tensor components(cpu_allocator(), dtypes[s], max_shapes[s]);
-          // components.setConstant(s);
-          out_tensors->emplace_back(std::move(components));
-        }
-
-        *end_of_sequence = false;
-
-        return Status::OK();
-      }
-
-     protected:
-      Status SaveInternal(IteratorStateWriter* writer) override {
-        mutex_lock l(mu_);
-        // TF_RETURN_IF_ERROR(writer->WriteScalar(full_name("next"), next_));
-        return Status::OK();
-      }
-
-      Status RestoreInternal(IteratorContext* ctx,
-                             IteratorStateReader* reader) override {
-        mutex_lock l(mu_);
-        // TF_RETURN_IF_ERROR(reader->ReadScalar(full_name("next"), &next_));
-        return Status::OK();
-      }
-
-     private:
-      mutex mu_;
-      // int64 next_ GUARDED_BY(mu_);
-    };
   };
 };
 
-REGISTER_OP("QueuedTensorDataset")
+REGISTER_OP("QueueDataset")
+    .Input("queue_handle: resource")
     .Output("handle: variant")
-    .Attr("Toutput_types: list(type) >= 1")
-    .Attr("Toutput_shapes: list(shape) >= 1")
     .SetIsStateful()  // Source dataset ops must be marked
                       // stateful to inhibit constant folding.
-    .SetShapeFn(shape_inference::ScalarShape);  // TODO(mrry): Validate that
-                                                // `components` have shapes
-                                                // compatible with
-// `output_shapes`.
-REGISTER_KERNEL_BUILDER(Name("QueuedTensorDataset").Device(DEVICE_CPU),
-                        QueuedTensorDatasetOp);
+    .SetShapeFn(shape_inference::ScalarShape);
+
+REGISTER_KERNEL_BUILDER(Name("QueueDataset").Device(DEVICE_CPU),
+                        QueueDatasetOp);
 
 }  // namespace
 
---
+++ montblanc/impl/rime/tensorflow/rime_ops/test_simple_queue_dataset.py
@@ -0,0 +1,137 @@
+import unittest
+
+import numpy as np
+import tensorflow as tf
+
+class TestQueueTensorDataset(unittest.TestCase):
+
+    def setUp(self):
+        # Load the rime operation library
+        from montblanc.impl.rime.tensorflow import load_tf_lib
+        self.rime = load_tf_lib("./rime.so")
+
+    def test_queue_tensor_dataset(self):
+
+        # TODO(sjperkins).
+        # Move QueueDataset into own python file.
+        from tensorflow.python.data.ops import iterator_ops
+        from tensorflow.python.data.util import nest
+        from tensorflow.python.data.util import random_seed
+        from tensorflow.python.data.util import sparse
+        from tensorflow.python.eager import context
+        from tensorflow.python.framework import constant_op
+        from tensorflow.python.framework import dtypes
+        from tensorflow.python.framework import function
+        from tensorflow.python.framework import ops
+        from tensorflow.python.framework import sparse_tensor as sparse_tensor_lib
+        from tensorflow.python.framework import tensor_shape
+        from tensorflow.python.framework import tensor_util
+        from tensorflow.python.ops import array_ops
+        from tensorflow.python.ops import gen_dataset_ops
+        from tensorflow.python.ops import gen_io_ops
+        from tensorflow.python.ops import math_ops
+        from tensorflow.python.ops import script_ops
+        from tensorflow.python.util import deprecation
+        from tensorflow.python.util.tf_export import tf_export
+
+        # HACK
+        thang = self
+
+        class TensorQueue(object):
+            def __init__(self, dtypes, shapes=None):
+                with ops.name_scope("tensors"):
+                    if isinstance(dtypes, tuple):
+                        pass
+                    elif isinstance(dtypes, list):
+                        dtypes = tuple(dtypes)
+                    else:
+                        dtypes = (dtypes,)
+
+                    self.output_types = dtypes
+
+                    if shapes is not None:
+                        assert len(shapes) == len(dtypes)
+
+                        self.output_shapes = shapes
+                    else:
+                        self.output_shapes = tuple(tensor_shape.unknown_shape() for dt in self.output_types)
+
+                self.output_classes = tuple(ops.Tensor for dt in self.output_types)
+                self.handle = thang.rime.dataset_queue_handle(self.output_types, self.output_shapes)
+
+            def put(self, tensors):
+                return thang.rime.dataset_queue_enqueue(self.handle, tensors)
+
+            def close(self):
+                return thang.rime.dataset_queue_close(self.handle)
+
+        class QueueDataset(tf.data.Dataset):
+          """A `Dataset` consuming elements from a queue"""
+
+          def __init__(self, queue):
+            super(QueueDataset, self).__init__()
+            self._queue = queue
+
+          def _as_variant_tensor(self):
+            return thang.rime.queue_dataset(self._queue.handle)
+
+          @property
+          def output_shapes(self):
+            return self._queue.output_shapes
+
+          @property
+          def output_types(self):
+            return self._queue.output_types
+
+          @property
+          def output_classes(self):
+            return self._queue.output_classes
+
+        with tf.Graph().as_default() as graph:
+            ci = tf.placeholder(dtype=tf.int64)
+            cf = tf.placeholder(dtype=tf.float64)
+
+            queue = TensorQueue([tf.int64, tf.float64])
+            ds = QueueDataset(queue)
+
+            put_op = queue.put([ci, cf])
+            close_op =queue.close()
+
+            it = ds.make_initializable_iterator()
+            next_op = it.get_next()
+
+            global_init_op = tf.global_variables_initializer()
+
+        with tf.Session(graph=graph) as S:
+            S.run([global_init_op, it.initializer])
+
+            import threading
+
+            N = 3
+
+            def _enqueue(n):
+                for i in  range(1, n+1):
+                    S.run(put_op, feed_dict={ci: [i]*i, cf: [i]*i})
+
+                S.run(close_op)
+
+            t = threading.Thread(target=_enqueue, args=(N,))
+            t.setDaemon(True)
+            t.start()
+
+            for i in range(1, N+1):
+                data = [i]*i
+
+                np_ints = np.asarray(data, dtype=np.int64)
+                np_floats = np.asarray(data, dtype=np.float64)
+
+                tf_ints, tf_floats = S.run(next_op)
+
+                self.assertTrue(np.all(np_ints == tf_ints))
+                self.assertTrue(np.all(np_floats == tf_floats))
+
+            with self.assertRaises(tf.errors.OutOfRangeError) as cm:
+                S.run(next_op)
+
+if __name__ == "__main__":
+    unittest.main()
