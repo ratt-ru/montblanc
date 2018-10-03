32cfd0bd0989d7b3a908d41b646dd31a96b49dc0
Simon Perkins <simon.perkins@gmail.com>
2018-10-03 12:45:18 +0200

    Setup dask inputs and top call for rime function

--- montblanc/impl/rime/tensorflow/tests/test_tf_session_wrapper.py
+++ montblanc/impl/rime/tensorflow/tests/test_tf_session_wrapper.py
@@ -1,7 +1,11 @@
-import cloudpickle
-import pytest
+from collections import Mapping
 
+import cloudpickle
+import dask
+import dask.array as da
+from dask.sharedict import ShareDict
 import numpy as np
+import pytest
 
 from montblanc.impl.rime.tensorflow.tf_session_wrapper import (
                                             TensorflowSessionWrapper)
@@ -65,3 +69,106 @@ def test_session_run(rime_cfg):
         # Check that input queue + map is clear
         assert w._session.run(in_ds.size) == 0
         assert w._session.run(pt_ds.size) == 0
+
+
+def _rime_factory(inputs):
+    try:
+        data_index = inputs.index("data")
+    except IndexError:
+        raise ValueError("This rime function depends on the use "
+                         "of the 'data' input within the rime function ")
+
+    def _rime(*args):
+        return args[data_index]
+
+    return _rime
+
+
+_fake_dim_chunks = {
+    'source': (5, 5, 5),
+    'row': (20, 20, 20, 20, 20),
+    'time': (1, 1, 1, 1, 1),
+    'chan': (16,),
+    'corr': (4,),
+    'ant': (7,),
+    '(u,v,w)': (3,),
+    '(l,m)': (2,)
+}
+
+
+def _fake_dask_inputs(input_data):
+    dask_inputs = []
+
+    for name, data in input_data:
+        chunks = tuple(_fake_dim_chunks[s] for s in data['schema'])
+        shape = tuple(map(sum, chunks))
+        dtype = data['type'].as_numpy_dtype()
+
+        array = da.random.random(size=shape, chunks=chunks).astype(dtype)
+        dask_inputs.append((name, data['schema'], array))
+
+    return dask_inputs
+
+
+def output_chunks(output_schema):
+    return tuple(_fake_dim_chunks[s] for s in output_schema)
+
+
+def _flatten_singletons(D):
+    """ Recursively simplify tuples and list of length 1 """
+
+    # lists and tuples should remain lists and tuples
+    if isinstance(D, list):
+        return (_flatten_singletons(D[0]) if len(D) == 1
+                else [_flatten_singletons(v) for v in D])
+    elif isinstance(D, tuple):
+        return (_flatten_singletons(D[0]) if len(D) == 1
+                else tuple(_flatten_singletons(v) for v in D))
+    elif isinstance(D, Mapping):
+        return {k: _flatten_singletons(v) for k, v in D.items()}
+    else:
+        return D
+
+
+def test_dask_wrap(rime_cfg):
+    with TensorflowSessionWrapper(basic, rime_cfg) as w:
+        inputs = []
+
+        for dsn, ds in w.placeholders.items():
+            inputs.extend(ds.items())
+
+        outputs = tuple((k, v['schema']) for k, v
+                        in w.placeholder_outputs.items())
+
+        inputs = sorted(inputs)
+        output_schema = max(outputs, key=lambda o: len(o[1]))[1]
+        # We're always producing this kind of output
+        output_schema = ["row", "chan", "corr"]
+
+        rime_fn = _rime_factory([name for name, _ in inputs])
+
+        dask_inputs = _fake_dask_inputs(inputs)
+
+        token = dask.base.tokenize(*(a for _, _, a in dask_inputs))
+        rime_name = "rime-" + token
+
+        name_schemas = [(a.name, s) for _, s, a in dask_inputs]
+        numblocks = {a.name: a.numblocks for _, _, a in dask_inputs}
+
+        rime_dsk = da.core.top(rime_fn, rime_name, output_schema,
+                               *(a for pair in name_schemas for a in pair),
+                               numblocks=numblocks)
+
+        rime_dsk = _flatten_singletons(rime_dsk)
+
+        dsk = ShareDict()
+        dsk.update(rime_dsk)
+
+        for _, _, a in dask_inputs:
+            dsk.update(a.__dask_graph__())
+
+        output = da.Array(dsk, rime_name,
+                          output_chunks(output_schema),
+                          dtype=np.complex128)
+
+        assert output.compute().shape == output.shape
