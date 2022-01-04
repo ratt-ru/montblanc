98bb44a195015e96351230199951be4101a8b968
bennahugo <bennahugo@gmail.com>
2022-01-04 11:40:59 +0200

    add test case to build without nvcc installed as would be needed for most downstream dependency tests

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -14,6 +14,9 @@ echo "----------------------------------------------"
 
 #build using docker file in directory:
 cd $PROJECTS_DIR/montblanc
-IMAGENAME="mb"
+IMAGENAME="mb_py38"
 docker build -t "$IMAGENAME:$BUILD_NUMBER" --no-cache=false -f .ci/py3.8.docker .
+IMAGENAME="mb_py36"
 docker build -t "$IMAGENAME:$BUILD_NUMBER" --no-cache=false -f .ci/py3.6.docker .
+IMAGENAME="mb_py38nonvss"
+docker build -t "$IMAGENAME:$BUILD_NUMBER" --no-cache=false -f .ci/withoutnvcc.py3.8.docker .
