3540ea7c3a85b7fa45a13b6b942c16e6cdfb2514
Benjamin Hugo <bennahugo@gmail.com>
2024-01-25 14:31:52 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -769,7 +769,7 @@ else:
 log.info('install_requires={}'.format(install_requires))
 
 setup(name='montblanc',
-    version="0.7.2.1",
+    version="0.7.3",
     description='GPU-accelerated RIME implementations.',
     long_description=readme(),
     url='http://github.com/ska-sa/montblanc',
